693c91456a00a48a2aef780959e6d2e6992ae73d
Tobias Oetiker <tobi@oetiker.ch>
2017-07-11 02:51:29 +0200

    remove excess / to make lint step work (#18)
    * remove excess / to make lint step work

--- lib/site.sh
+++ lib/site.sh
@@ -1,11 +1,11 @@
 # Package server URL and publisher
 : ${PKGPUBLISHER:=omnios}
-: ${PKGSRVR:=file:///$MYDIR/../tmp.repo/}
+: ${PKGSRVR:=file://$MYDIR/../tmp.repo/}
 
 # To create a on-disk repo in the top level of your checkout
 # and publish there instead of the URI specified above.
 #
-#PKGSRVR=file:///$MYDIR/../tmp.repo/
+#PKGSRVR=file://$MYDIR/../tmp.repo/
 
 # Uncommenting this line will use a pre-built illumos-omnios, instead of having
 # us build it.  NOTE: A build of illumos-omnios can be launched concurrently in
