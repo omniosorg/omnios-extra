edb2e63deb94726f5d50b33f611e276c5bfa4c02
Dominik Hassler <hadfl@omniosce.org>
2017-08-11 01:59:27 +0200

    security fixes for git and hg - CVE-2017-1000117 - CVE-2017-1000116

--- build/mercurial/build.sh
+++ build/mercurial/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=mercurial
-VER=4.2.2
+VER=4.2.3
 PKG=developer/versioning/mercurial
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
