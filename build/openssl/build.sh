e938ce77c227341f25edb67e8a41de79f1d2f4f9
citrus-it <omnios@citrus-it.co.uk>
2017-08-17 14:05:40 +0000

    Restructure openssl build

--- build/openssl/build.sh
+++ build/openssl/build.sh
@@ -22,6 +22,7 @@
 #
 #
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
 # Load support functions
@@ -34,7 +35,7 @@ PKG=library/security/openssl # Package name (without prefix)
 SUMMARY="$PROG - A toolkit for Secure Sockets Layer (SSL v2/v3) and Transport Layer (TLS v1) protocols and general purpose cryptographic library"
 DESC="$SUMMARY"
 
-DEPENDS_IPS="SUNWcs system/library system/library/gcc-5-runtime library/zlib@1.2.11"
+DEPENDS_IPS="SUNWcs system/library system/library/gcc-5-runtime library/zlib"
 BUILD_DEPENDS_IPS="$DEPENDS_IPS developer/sunstudio12.1"
 
 # Generic configure optons for both 32 and 64bit variants
@@ -54,18 +55,15 @@ OPENSSL_CONFIG_64_OPTS="enable-ec_nistp_64_gcc_128"
 NO_PARALLEL_MAKE=1
 
 make_prog() {
-    [[ -n $NO_PARALLEL_MAKE ]] && MAKE_JOBS=""
+    [ -n "$NO_PARALLEL_MAKE" ] && MAKE_JOBS=
     logmsg "--- make"
     # This will setup the internal runpath of libssl and libcrypto
     logcmd $MAKE $MAKE_JOBS SHARED_LDFLAGS="$SHARED_LDFLAGS" || \
         logerr "--- Make failed"
-    logmsg "--- make test"
-    logcmd $MAKE test || \
-        logerr "--- make test failed"
 }
 
 configure32() {
-    if [ -n "`isalist | grep sparc`" ]; then
+    if isalist | egrep -s sparc; then
       SSLPLAT=solaris-sparcv8-cc
     else
       SSLPLAT=solaris-x86-gcc
@@ -77,6 +75,7 @@ configure32() {
         || logerr "Failed to run configure"
     SHARED_LDFLAGS="-shared -Wl,-z,text"
 }
+
 configure64() {
     if [ -n "`isalist | grep sparc`" ]; then
       SSLPLAT=solaris64-sparcv9-cc
@@ -97,35 +96,13 @@ make_install() {
         logerr "Failed to make install"
 }
 
-# Move installed libs from /usr/lib to /lib and make symlinks to match upstream package
-move_libs() {
-    logmsg "link up certs"
-    logcmd rmdir $DESTDIR/usr/ssl/certs ||
-        logerr "Failed to remove /usr/ssl/certs"
-    logcmd ln -s ../../etc/ssl/certs $DESTDIR/usr/ssl/certs ||
-        logerr "Failed to link up /usr/ssl/certs -> /etc/ssl/certs"
-    logmsg "Relocating libs from usr/lib to lib"
-    logcmd mv $DESTDIR/usr/lib/64 $DESTDIR/usr/lib/amd64
-    logcmd mkdir -p $DESTDIR/lib/amd64
-    logcmd mv $DESTDIR/usr/lib/lib* $DESTDIR/lib/ ||
-        logerr "Failed to move libs (32-bit)"
-    logcmd mv $DESTDIR/usr/lib/amd64/lib* $DESTDIR/lib/amd64/ ||
-        logerr "Failed to move libs (64-bit)"
-    logmsg "--- Making usr/lib symlinks"
-    pushd $DESTDIR/usr/lib > /dev/null
-    logcmd ln -s ../../lib/libssl.so.1.0.0 libssl.so
-    logcmd ln -s ../../lib/libssl.so.1.0.0 libssl.so.1.0.0
-    logcmd ln -s ../../lib/libcrypto.so.1.0.0 libcrypto.so
-    logcmd ln -s ../../lib/libcrypto.so.1.0.0 libcrypto.so.1.0.0
-    popd > /dev/null
-    pushd $DESTDIR/usr/lib/amd64 > /dev/null
-    logcmd ln -s ../../../lib/amd64/libssl.so.1.0.0 libssl.so
-    logcmd ln -s ../../../lib/amd64/libssl.so.1.0.0 libssl.so.1.0.0
-    logcmd ln -s ../../../lib/amd64/libcrypto.so.1.0.0 libcrypto.so
-    logcmd ln -s ../../../lib/amd64/libcrypto.so.1.0.0 libcrypto.so.1.0.0
+install_pkcs11()
+{
+    logmsg "--- installing pkcs11 engine"
+    pushd $SRCDIR/pkcs11 > /dev/null
+    find . | cpio -pvmud $TMPDIR/$BUILDDIR/crypto/engine/
     popd > /dev/null
 }
-
 
 # Turn the letter component of the version into a number for IPS versioning
 ord26() {
@@ -134,6 +111,7 @@ ord26() {
     [[ $ASCII -gt 32 ]] && ASCII=$((ASCII - 32))
     echo $ASCII
 }
+
 save_function make_package make_package_orig
 make_package() {
     if [[ -n "`echo $VER | grep [a-z]`" ]]; then
@@ -145,14 +123,33 @@ make_package() {
     make_package_orig
 }
 
+# Move installed libs from /usr/lib to /lib
+move_libs() {
+    logmsg "Relocating libs from usr/lib to lib"
+    logcmd mv $DESTDIR/usr/lib/64 $DESTDIR/usr/lib/amd64
+    logcmd mkdir -p $DESTDIR/lib/amd64
+    logcmd mv $DESTDIR/usr/lib/lib* $DESTDIR/lib/ ||
+        logerr "Failed to move libs (32-bit)"
+    logcmd mv $DESTDIR/usr/lib/amd64/lib* $DESTDIR/lib/amd64/ ||
+        logerr "Failed to move libs (64-bit)"
+}
+
+######################################################################
+
 init
+
+### Openssl 1.0.x build
 download_source $PROG $PROG $VER
 patch_source
+install_pkcs11
 prep_build
 build
+run_testsuite
 move_libs
+
 make_lintlibs crypto /lib /usr/include "openssl/!(ssl*|*tls*).h"
 make_lintlibs ssl /lib /usr/include "openssl/{ssl,*tls}*.h"
 make_isa_stub
 make_package
 clean_up
+
