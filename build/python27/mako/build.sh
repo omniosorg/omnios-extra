2d5b72b2dbafac3f9fe35f01058135aee81a5e18
citrus-it <omnios@citrus-it.co.uk>
2017-09-01 18:16:32 +0000

    mako: update to 1.0.7

--- build/python27/mako/build.sh
+++ build/python27/mako/build.sh
@@ -25,19 +25,18 @@
 # Use is subject to license terms.
 #
 # Load support functions
-. ../../lib/functions.sh
+. ../../../lib/functions.sh
 
+PKG=library/python-2/mako-27
 PROG=Mako
-VER=1.0.6
+VER=1.0.7
 SUMMARY="Mako - a python templating language"
 DESC="$SUMMARY"
 
-# In the future when we upgrade python again, be sure to wrap the following
-# around with set_python_version and reassign PKG and RUN_DEPENDS_IPS.
-# The only way buildctl detects packages is by grepping for PKG assignment.
-XFORM_ARGS="-D PYTHONVER=$PYTHONVER"
-PKG=library/python-2/mako-27
-RUN_DEPENDS_IPS="runtime/python-27 library/python-2/setuptools-27"
+. $SRCDIR/../common.sh
+
+RUN_DEPENDS_IPS+="library/python-2/setuptools-27"
+
 init
 download_source $PROG $PROG $VER
 patch_source
