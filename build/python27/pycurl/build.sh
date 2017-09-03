55ec3f2e332db198653a0cc6f59518226ec1a2bc
citrus-it <omnios@citrus-it.co.uk>
2017-09-03 22:07:48 +0000

    Update copyrights for OmniOSce

--- build/python27/pycurl/build.sh
+++ build/python27/pycurl/build.sh
@@ -22,6 +22,7 @@
 #
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
 # Load support functions
