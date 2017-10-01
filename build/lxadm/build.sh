f3334269f19a98c26a1214ea0b9772935f9cf9ce
Dominik Hassler <hadfl@omniosce.org>
2017-10-01 17:02:07 +0200

    lxadm added

---
+++ build/lxadm/build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=lxadm      # App name
+VER=0.1.5      # App version
+VERHUMAN=$VER   # Human-readable version
+#PVER=          # Branch (set in config.sh, override here if needed)
+PKG=ooce/lxadm  # Package name (e.g. library/foo)
+SUMMARY="Manage illumos LX zones" # One-liner, must be filled in
+DESC=$SUMMARY   # Longer description, must be filled in
+BUILDARCH=32    # or 64 or both ... for libraries we want both for tools 32 bit only
+PREFIX=/opt/ooce
+MIRROR="https://github.com/hadfl/$PROG/releases/download"
+RUN_DEPENDS_IPS="system/zones/brand/lx"
+XFORM_ARGS="-D PREFIX=$PREFIX"
+
+CONFIGURE_OPTS_32="
+    --prefix=$PREFIX/$PROG
+    --localstatedir=/var$PREFIX/$PROG"
+
+init
+download_source "v$VER" $PROG $VER
+patch_source
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
