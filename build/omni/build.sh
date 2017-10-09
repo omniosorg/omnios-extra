4542bb74f5c2f2aba6b70a2b5d34992903b65fbc
Andy Fiddaman <omnios@citrus-it.co.uk>
2017-10-09 14:00:04 +0000

    omni: add to r151022 branch

---
+++ build/omni/build.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=omni
+VER=1.1.0
+VERHUMAN=$VER
+PKG=ooce/developer/omni
+SUMMARY="OmniOS build management utility"
+DESC=$SUMMARY
+PREFIX=/opt/ooce
+MIRROR="https://github.com/omniosorg/$PROG/archive"
+BUILDDIR="$PROG-$VER"
+TAR=gtar
+XFORM_ARGS="-D PREFIX=$PREFIX"
+
+build() {
+    mkdir -p "$DESTDIR/$PREFIX/$PROG"
+    ( cd $TMPDIR/$BUILDDIR; find . | cpio -pvmud "$DESTDIR/$PREFIX/$PROG/" )
+}
+
+init
+download_source $VER $VER ""
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
