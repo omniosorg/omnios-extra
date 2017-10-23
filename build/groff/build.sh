73feba8cdcf21e492eb31ca228938c7a42a0a73a
Andy Fiddaman <omnios@citrus-it.co.uk>
2017-10-23 14:53:03 +0000

    groff: clean-up

--- build/groff/build.sh
+++ build/groff/build.sh
@@ -22,6 +22,7 @@
 #
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
 # Load support functions
@@ -36,48 +37,29 @@ DESC="$SUMMARY"
 # As of 1.22.3, parallel gmake breaks.
 NO_PARALLEL_MAKE=1
 
-DEPENDS_IPS="SUNWcs system/library/gcc-5-runtime system/library/g++-5-runtime
-	runtime/perl system/library/math system/library"
+RUN_DEPENDS_IPS="prerequisite/gnu"
 
 BUILDARCH=32
-CONFIGURE_OPTS_32="--prefix=$PREFIX
-        --sysconfdir=/etc
-        --includedir=$PREFIX/include
-        --bindir=$PREFIX/bin
-        --sbindir=$PREFIX/sbin
-        --libdir=$PREFIX/lib
-        --libexecdir=$PREFIX/libexec"
+CONFIGURE_OPTS_32="
+    --prefix=$PREFIX
+    --sysconfdir=/etc
+    --includedir=$PREFIX/include
+    --bindir=$PREFIX/bin
+    --sbindir=$PREFIX/sbin
+    --libdir=$PREFIX/lib
+    --libexecdir=$PREFIX/libexec
+"
 CONFIGURE_OPTS="--without-x"
-
-cleanup_gnuism() {
-    GNUCLASH="diffmk eqn grn indxbib neqn nroff pic refer soelim"
-    mkdir -p $DESTDIR/usr/gnu/bin
-    for clash in $GNUCLASH ; do
-        ln -s ../../bin/g$clash $DESTDIR/usr/gnu/bin/$clash
-    done
-}
-
-install_license() {
-    local LICENSE_FILE
-    LICENSE_FILE=$TMPDIR/$BUILDDIR/$1
-
-    if [ -f "$LICENSE_FILE" ]; then
-        logmsg "Using $LICENSE_FILE as package license"
-        logcmd cp $LICENSE_FILE $DESTDIR/license
-    else
-        logerr "-- $LICENSE_FILE not found!"
-        exit 255
-    fi
-}
 
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
-install_license COPYING
 make_isa_stub
 strip_install
-cleanup_gnuism
 make_package
 clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
