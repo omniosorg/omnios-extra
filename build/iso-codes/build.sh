f31f887dcab67b4bbbac2e815b645d88663f967b
Andy Fiddaman <omnios@citrus-it.co.uk>
2017-11-22 15:14:01 +0000

    iso-codes: update to 3.77

--- build/iso-codes/build.sh
+++ build/iso-codes/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# CDDL HEADER START
+# {{{ CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
@@ -18,17 +18,17 @@
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
 #
-# CDDL HEADER END
-#
+# CDDL HEADER END }}}
 #
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=iso-codes
-VER=3.76
+VER=3.77
 VERHUMAN=$VER
 PKG=data/iso-codes
 SUMMARY="ISO code lists and translations"
@@ -40,21 +40,14 @@ export PATH=/opt/ooce/bin:$PATH
 
 BUILDARCH=32
 
-# Upstream doesn't ship any of the translations, so we won't either
-remove_translations() {
-    logmsg "Removing translation files"
-    logcmd rm -rf $DESTDIR$PREFIX/share/locale
-}
-
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
-remove_translations
 make_isa_stub
 make_package
 clean_up
 
 # Vim hints
-# vim:ts=4:sw=4:et:
+# vim:ts=4:sw=4:et:fdm=marker
