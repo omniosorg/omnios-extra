e70d698d8abd7a3fa2225949d381316226660bfe
Andy Fiddaman <omnios@citrus-it.co.uk>
2017-12-01 16:35:01 +0000

    vim: Fix CVE-2017-17087

--- build/vim/build.sh
+++ build/vim/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# CDDL HEADER START
+# {{ CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
@@ -18,8 +18,7 @@
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
 #
-# CDDL HEADER END
-#
+# CDDL HEADER END }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
@@ -29,6 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=vim
+# This is the last available bundled version
 VER=8.0.586
 PKG=editor/vim
 SUMMARY="Vi IMproved"
@@ -50,7 +50,6 @@ CONFIGURE_OPTS="
     --disable-gui
     --disable-gtktest
 "
-reset_configure_opts
 
 init
 download_source $PROG $PROG $VER
@@ -62,4 +61,4 @@ make_package
 clean_up
 
 # Vim hints
-# vim:ts=4:sw=4:et:
+# vim:ts=4:sw=4:et:fdm=marker
