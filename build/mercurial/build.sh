f97c99b3cffd7c5bec209d8feac40edbe82b8a46
Dominik Hassler <hadfl@omniosce.org>
2017-12-03 13:59:49 +0100

    hg: update to 4.4.2

--- build/mercurial/build.sh
+++ build/mercurial/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=mercurial
-VER=4.4.1
+VER=4.4.2
 PKG=developer/versioning/mercurial
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
