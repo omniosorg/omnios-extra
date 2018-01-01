2b3e70a93043bbe56ac77c56a1ee585b5cc9de6a
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-01-01 19:01:42 +0000

    dtrace toolkit updates

--- build/dtrace-toolkit/build.sh
+++ build/dtrace-toolkit/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# CDDL HEADER START
+# {{{ CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
@@ -18,13 +18,12 @@
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
 #
-# CDDL HEADER END
-#
+# CDDL HEADER END }}}
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
-# Load support functions
 . ../../lib/functions.sh
 
 PROG=DTraceToolkit
@@ -33,16 +32,17 @@ PKG=developer/dtrace/toolkit
 SUMMARY="$PROG ($VER)"
 DESC="$PROG - a collection of over 200 useful and documented DTrace scripts"
 
-DEPENDS_IPS="developer/dtrace runtime/perl runtime/python-27"
+DEPENDS_IPS="runtime/python-27"
 
 PREFIX=/opt/DTT
 
 # The toolkit is just scripts, so there is nothing to compile
 build_toolkit() {
-  logmsg "Installing contents to packaging directory $DESTDIR/$PREFIX"
-  logcmd mkdir -p $DESTDIR/$PREFIX || logerr "--- Could not create packaging directory"
-  logcmd cp -rpP $TMPDIR/$BUILDDIR/* $DESTDIR/$PREFIX/ || logerr "--- Install failed."
-  logcmd rm -f $DESTDIR/$PREFIX/install || logerr "--- Failed to remove the install script that we don't use."
+    logmsg "Installing contents to packaging directory $DESTDIR/$PREFIX"
+    logcmd mkdir -p $DESTDIR/$PREFIX \
+        || logerr "--- Could not create packaging directory"
+    logcmd cp -rpP $TMPDIR/$BUILDDIR/* $DESTDIR/$PREFIX/ \
+        || logerr "--- Install failed."
 }
 
 init
@@ -52,3 +52,6 @@ prep_build
 build_toolkit
 make_package
 clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
