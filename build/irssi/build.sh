6e8adbfa4b9181bf85c201c4b4f385b6f06eb70e
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-02-10 12:57:05 +0000

    Add irssi package

---
+++ build/irssi/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+#
+# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
+# Use is subject to license terms.
+#
+. ../../lib/functions.sh
+
+PROG=irssi
+VER=1.1.0
+VERHUMAN=$VER
+PKG=ooce/network/irssi
+SUMMARY="$PROG - text-mode modular chat client"
+DESC="$SUMMARY"
+
+OPREFIX=$PREFIX
+PREFIX+="/$PROG"
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+    -DOPREFIX=${OPREFIX#/}
+    -DPROG=$PROG
+"
+
+reset_configure_opts
+
+# Build 64-bit only and skip the arch-specific directories
+BUILDARCH=64
+CONFIGURE_OPTS="
+    --bindir=$PREFIX/bin
+    --libdir=$PREFIX/lib
+"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
