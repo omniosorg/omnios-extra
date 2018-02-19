c04a35312428e75b054e522e58c27e8854c9e752
Dominik Hassler <hadfl@omniosce.org>
2018-02-19 23:08:18 +0100

    pkgmgr: update to 0.2.4

--- build/pkgmgr/build.sh
+++ build/pkgmgr/build.sh
@@ -23,11 +23,10 @@
 # Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
-# Load support functions
 . ../../lib/functions.sh
 
 PROG=pkgmgr
-VER=0.2.3
+VER=0.2.4
 VERHUMAN=$VER
 PKG=ooce/developer/pkgmgr
 SUMMARY="IPS package management/publishing tool"
@@ -37,31 +36,23 @@ MIRROR="https://github.com/omniosorg/$PROG/releases/download"
 
 RUN_DEPENDS_IPS="network/rsync"
 
-CONFIGURE_OPTS_32="
-    --prefix=$PREFIX/$PROG
-    --sysconfdir=/etc$PREFIX/$PROG
-"
+OPREFIX=$PREFIX
+PREFIX+="/$PROG"
 
-add_extra_files() {
-    # create symbolic link to standard bin dir
-    logcmd mkdir -p $DESTDIR/$PREFIX/bin
-    logcmd ln -s ../$PROG/bin/$PROG $DESTDIR/$PREFIX/bin/$PROG \
-        || logerr "--- cannot create $PROG symlink"
-    # create symbolic link to man page
-    logcmd mkdir -p $DESTDIR/$PREFIX/share/man/man1
-    logcmd ln -s ../../../$PROG/share/man/man1/${PROG}.1 $DESTDIR/$PREFIX/share/man/man1/${PROG}.1 \
-        || logerr "--- cannot create man page symlink"
-    # copy config template
-    logcmd cp $DESTDIR/etc/$PREFIX/$PROG/${PROG}.conf.dist $DESTDIR/etc/$PREFIX/$PROG/${PROG}.conf \
-        || logerr "--- cannot copy config file template"
-}
+XFORM_ARGS="-DOPREFIX=$OPREFIX -DPROG=$PROG"
+
+reset_configure_opts
+
+CONFIGURE_OPTS_32="
+    --prefix=$PREFIX
+    --sysconfdir=/etc$PREFIX
+"
 
 init
 download_source "v$VER" $PROG $VER
 patch_source
 prep_build
 build
-add_extra_files
 make_package
 clean_up
 
