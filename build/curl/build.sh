abb384c9a11a8681124f52806668f9bbbcd44dec
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-03-14 09:11:21 +0000

    curl: update to 7.59.0

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -29,7 +29,7 @@
 . ../../lib/functions.sh
 
 PROG=curl       # App name
-VER=7.58.0      # App version
+VER=7.59.0      # App version
 PKG=web/curl    # Package name (without prefix)
 SUMMARY="$PROG - command line tool for transferring data with URL syntax"
 DESC="$SUMMARY"
