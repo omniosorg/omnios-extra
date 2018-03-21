0bdba4e872d9354655016e2e42c9e053dc861b30
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-03-21 11:34:30 +0000

    pcre: update to 8.42

--- build/pcre/build.sh
+++ build/pcre/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# CDDL HEADER START
+# {{{ CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
@@ -18,29 +18,32 @@
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
 #
-# CDDL HEADER END
-#
+# CDDL HEADER END }}}
+
 #
 # Copyright 2011-2015 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
-# Load support functions
 . ../../lib/functions.sh
 
 PROG=pcre
-VER=8.41
+VER=8.42
 VERHUMAN=$VER
 PKG=library/pcre
 SUMMARY="Perl-Compatible Regular Expressions"
-DESC="PCRE - Perl-Compatible Regular Expressions"
+DESC="PCRE - $SUMMARY"
 
-DEPENDS_IPS="library/readline compress/bzip2 library/zlib system/library/g++-runtime
-	system/library/gcc-runtime system/library system/library/math"
+RUN_DEPENDS_IPS="
+    library/readline
+    compress/bzip2
+    library/zlib
+"
 
 LIBTOOL_NOSTDLIB=libtool
 LIBTOOL_NOSTDLIB_EXTRAS=-lc
 
-CONFIGURE_OPTS="$CONFIGURE_OPTS
+CONFIGURE_OPTS+="
 	--includedir=/usr/include/pcre
 	--localstatedir=/var
 	--disable-static
@@ -76,4 +79,4 @@ make_package
 clean_up
 
 # Vim hints
-# vim:ts=4:sw=4:et:
+# vim:ts=4:sw=4:et:fdm=marker
