d456a75b238b1d3bb0cb71d494c7f2b2284fc90f
Dominik Hassler <hadfl@omniosce.org>
2018-03-27 20:08:02 +0200

    openssl: update to 1.1.0h/1.0.2o

--- build/openssl/build.sh
+++ build/openssl/build.sh
@@ -29,8 +29,8 @@
 . ../../lib/functions.sh
 
 PROG=openssl
-VER=1.1.0g
-LVER=1.0.2n
+VER=1.1.0h
+LVER=1.0.2o
 VERHUMAN=$VER
 PKG=library/security/openssl
 SUMMARY="$PROG - A toolkit for Secure Sockets Layer and Transport Layer protocols and general purpose cryptographic library"
