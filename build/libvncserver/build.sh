d40d1041a96b60f70a5adb12fd31c2cd900d4b51
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-11-18 09:52:04 +0000

    Add libvncserver package

---
+++ build/libvncserver/build.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/functions.sh
+
+PROG=libvncserver
+VER=20181116
+PKG=ooce/library/libvncserver
+SUMMARY="libvncserver"
+DESC="A library for easy implementation of a VNC server."
+
+BUILDDIR=$PROG-master
+
+BUILD_DEPENDS_IPS="
+    ooce/developer/cmake
+    ooce/library/libjpeg-turbo
+"
+
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+    -DPROG=$PROG
+"
+
+CONFIGURE_CMD="$PREFIX/bin/cmake"
+
+# Run these early to make DESTDIR available
+init
+prep_build
+
+CONFIGURE_OPTS="
+    ..
+    -DENABLE_STATIC=0
+    -DADDITIONAL_LIBS=socket
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+"
+CONFIGURE_OPTS_64="
+    -DJPEG_LIBRARY_RELEASE:FILEPATH=$PREFIX/lib/amd64/libjpeg.so
+    -DPNG_LIBRARY_RELEASE:FILEPATH=$PREFIX/lib/amd64/libpng.so
+"
+
+LDFLAGS32+=" -L$PREFIX/lib -R$PREFIX/lib"
+LDFLAGS64+=" -L$PREFIX/lib/amd64 -R$PREFIX/lib/amd64"
+
+BUILDORDER="64 32"
+
+save_function make_install64 _make_install64
+make_install64() {
+    _make_install64
+    pushd $DESTDIR/$PREFIX >/dev/null
+    logcmd mkdir -p lib/amd64/
+    logcmd mv lib/*.so.* lib/pkgconfig lib/amd64/
+    popd >/dev/null
+}
+
+build() {
+    _BUILDDIR=$BUILDDIR
+    for b in $BUILDORDER; do
+        mkdir -p $TMPDIR/$BUILDDIR/build.$b
+        BUILDDIR+="/build.$b"
+        [[ $BUILDARCH =~ ^($b|both)$ ]] && build$b
+        BUILDDIR=$_BUILDDIR
+    done
+}
+
+download_source $PROG $PROG master-$VER
+patch_source
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
