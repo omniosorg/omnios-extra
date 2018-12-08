5d1e74781b1437f289d94a48c3d338385b2a7179
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-12-08 10:41:44 +0000

    virtualbox: add vboxwebsrv

--- build/virtualbox/build.sh
+++ build/virtualbox/build.sh
@@ -19,6 +19,8 @@
 PROG=VirtualBox
 PKG=ooce/virtualization/virtualbox
 VER=5.2.22
+GSOAPVER=2.8.73
+GSOAPDIR=gsoap-${GSOAPVER%.*}
 SUMMARY="VirtualBox"
 DESC="VirtualBox is a general-purpose full virtualiser for x86 hardware, "
 DESC+="targeted at server, desktop and embedded use."
@@ -70,6 +72,26 @@ XFORM_ARGS="
 # The usual --prefix etc. options are not supported
 CONFIGURE_OPTS_64=
 
+init
+prep_build
+
+#########################################################################
+
+# Download and build a static version of gsoap which is required for the
+# vboxwebservice build
+CONFIGURE_OPTS="
+    --prefix=/usr
+    --enable-ipv6
+"
+# gsoap does not build with parallel make
+NO_PARALLEL_MAKE=1
+build_dependency gsoap $GSOAPDIR gsoap gsoap_$GSOAPVER ""
+NO_PARALLEL_MAKE=
+LD_LIBRARY_PATH+=":$DEPROOT/usr/lib"
+export LD_LIBRARY_PATH
+
+#########################################################################
+
 CONFIGURE_OPTS="
     --build-headless
     --build-libopus
@@ -80,6 +102,7 @@ CONFIGURE_OPTS="
     --disable-dbus
     --disable-kmods
     --enable-vnc
+    --enable-webservice
 "
 
 make_prog() {
@@ -105,6 +128,16 @@ VBOX_WITH_DTRACE_R0DRV =
 VBOX_WITH_DTRACE_RC =
 VBOX_WITH_NATIVE_DTRACE =
 VBoxVNC_INCS = /opt/ooce/include
+
+NOWEBSVC =
+VBOX_WITH_WEBSERVICES = 1
+VBOX_WITH_WEBSERVICES_SSL = 1
+VBOX_GSOAP_INSTALLED = 1
+VBOX_PATH_GSOAP = $DEPROOT/usr
+VBOX_PATH_GSOAP_IMPORT = $DEPROOT/usr/share/gsoap/import
+VBOX_GSOAP_CXX_SOURCES =
+VBOX_GSOAP_CXX_LIBS = libgsoapssl++ libz
+VBOX_GSOAP_INCS = $DEPROOT/usr/include
 
 EOF
     logmsg "--- building VirtualBox"
@@ -148,10 +181,8 @@ make_install() {
         $DESTDIR$PREFIX/extpack
 }
 
-init
 download_source $PROG $PROG $VER
 patch_source
-prep_build
 build
 make_package
 clean_up
