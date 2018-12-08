1c5e5c0fbd3469c945fd039612cbe42b6b20c944
Dominik Hassler <hadfl@omniosce.org>
2018-12-08 13:12:23 +0100

    php 7.3.x added

--- build/php/build-73.sh
+++ build/php/build-73.sh
@@ -18,10 +18,10 @@
 . ../../lib/functions.sh
 
 PROG=php
-PKG=ooce/application/php-72
-VER=7.2.12
+PKG=ooce/application/php-73
+VER=7.3.0
 VERHUMAN=$VER
-SUMMARY="PHP 7.2"
+SUMMARY="PHP 7.3"
 DESC="A popular general-purpose scripting language"
 
 BUILDARCH=64
