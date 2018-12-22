836a071dbf6cd1f67357654fdbc9a2d1a42e66a2
Dominik Hassler <hadfl@omniosce.org>
2018-12-22 13:58:30 +0100

    cmake: update to 3.13.2

--- build/cmake/build.sh
+++ build/cmake/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=cmake
-VER=3.12.3
+VER=3.13.2
 PKG=ooce/developer/cmake
 SUMMARY="Build coordinator"
 DESC="An extensible system that manages the build process in a "
