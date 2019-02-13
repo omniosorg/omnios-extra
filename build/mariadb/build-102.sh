19df6df37db5a663f5e4549b9b0e0ec6b872848b
Dominik Hassler <hadfl@omniosce.org>
2019-02-13 21:05:50 +0100

    mariadb-102: update to 10.2.22

--- build/mariadb/build-102.sh
+++ build/mariadb/build-102.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=mariadb
-VER=10.2.21
+VER=10.2.22
 PKG=ooce/database/mariadb-102
 SUMMARY="MariaDB"
 DESC="A community-developed, commercially supported fork of the "
