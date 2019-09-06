7fb05aef1a2069dfd1f97108b873baf7f1905056
Dominik Hassler <hadfl@omniosce.org>
2019-09-06 18:55:55 +0000

    virtualbox: update to 6.0.12

--- build/virtualbox/build6.sh
+++ build/virtualbox/build6.sh
@@ -18,8 +18,8 @@
 
 PROG=VirtualBox
 PKG=ooce/virtualization/virtualbox
-VER=6.0.10
-GSOAPVER=2.8.87
+VER=6.0.12
+GSOAPVER=2.8.91
 GSOAPDIR=gsoap-${GSOAPVER%.*}
 SUMMARY="VirtualBox"
 DESC="VirtualBox is a general-purpose full virtualiser for x86 hardware, "
