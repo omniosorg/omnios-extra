92bde55a992d1abca486b9d00fd036371f0c0373
Dominik Hassler <hadfl@omniosce.org>
2019-10-04 17:29:07 +0000

    znapzend added

---
+++ build/znapzend/build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+#
+# Copyright 1995-2013 OETIKER+PARTNER AG  All rights reserved.
+# Copyright 2019 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/functions.sh
+
+PROG=znapzend
+VER=0.19.1
+PKG=ooce/system/znapzend
+SUMMARY="A ZFS-aware backup script"
+DESC="Take snapshots and transfer them to a second pool, "
+DESC+="potentially on a different box"
+
+RUN_DEPENDS_IPS="runtime/perl-64"
+
+OPREFIX=$PREFIX
+PREFIX+="/$PROG"
+
+set_arch 64
+
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+    -DOPREFIX=${OPREFIX#/}
+    -DPROG=$PROG
+"
+
+CONFIGURE_OPTS_64="
+    --prefix=$PREFIX
+"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+install_smf system system-$PROG.xml
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
