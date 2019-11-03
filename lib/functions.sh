7169ff9ed285453b5d2e8b0c8990a9debb623d28
Andy Fiddaman <omnios@citrus-it.co.uk>
2019-11-03 21:25:44 +0000

    Validate SMF manifests

--- lib/functions.sh
+++ lib/functions.sh
@@ -1399,6 +1399,9 @@ install_smf() {
     pushd $DESTDIR > /dev/null
     logmsg "-- Installing SMF service ($mtype / $manifest / $method)"
 
+    logcmd svccfg validate $SRCDIR/files/$manifest \
+        || logerr "Manifest does not pass validation"
+
     # Manifest
     logcmd mkdir -p lib/svc/manifest/$mtype \
         || logerr "mkdir of $DESTDIR/lib/svc/manifest/$mtype failed"
