eb92f5e64b90cfe8d1892314776ecaab29f70299
Dominik Hassler <hadfl@omniosce.org>
2019-11-26 18:19:55 +0000

    protobuf: update to 3.11.0

--- build/protobuf/build.sh
+++ build/protobuf/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=protobuf
-VER=3.10.0
+VER=3.11.0
 PKG=ooce/library/protobuf
 SUMMARY="protobuf"
 DESC="Google's language-neutral, platform-neutral, extensible mechanism "
