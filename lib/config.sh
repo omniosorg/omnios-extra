db3017e7156fa112ccebc169f7c632c70728de50
Dominik Hassler <hadfl@omniosce.org>
2020-01-30 18:21:47 +0000

    add variable that holds the same gcc version as illumos-omnios

--- lib/config.sh
+++ lib/config.sh
@@ -238,12 +238,12 @@ CXX=g++
 
 # Specify default GCC version for building packages
 case $RELVER in
-    15102[12])          DEFAULT_GCC_VER=5.1.0 ;;
+    15102[12])          DEFAULT_GCC_VER=5.1.0; ILLUMOS_GCC_VER=4.4.4 ;;
     15102[34])          DEFAULT_GCC_VER=5 ;;
     15102[56])          DEFAULT_GCC_VER=6 ;;
     15102[78])          DEFAULT_GCC_VER=7 ;;
-    151029|15103[0-2])  DEFAULT_GCC_VER=8 ;;
-    151033)             DEFAULT_GCC_VER=9 ;;
+    151029|15103[0-2])  DEFAULT_GCC_VER=8; ILLUMOS_GCC_VER=7 ;;
+    151033)             DEFAULT_GCC_VER=9; ILLUMOS_GCC_VER=7 ;;
     *) logerr "Unknown release '$RELVER', can't select compiler." ;;
 esac
 
