0ac4e54a109659ea6d9e203c1dbc1544b179cdab
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-02-07 13:41:45 +0000

    Use the published repo as a source when linting

--- lib/functions.sh
+++ lib/functions.sh
@@ -1079,11 +1079,14 @@ EOM
             done
             echo
         ) > $BASE_TMPDIR/lint/pkglintrc
-        _repo="-r $repo"
+        [ $RELVER -ge 151033 ] \
+            && _repo="-r $repo -r $IPS_REPO -r $OB_IPS_REPO" \
+            || _repo="-r $repo"
     fi
     echo $c_note
-    $PKGLINT -f $BASE_TMPDIR/lint/pkglintrc -c $BASE_TMPDIR/lint/cache $mf \
-        $_repo || logerr "----- pkglint failed"
+    logcmd -p $PKGLINT -f $BASE_TMPDIR/lint/pkglintrc \
+        -c $BASE_TMPDIR/lint/cache $mf $_repo \
+        || logerr "----- pkglint failed"
     echo $c_reset
 }
 
