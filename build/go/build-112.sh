2715e195922ead50510f248142c6a9f3dfe3df63
Dominik Hassler <hadfl@omniosce.org>
2020-02-13 20:54:59 +0000

    go-112: update to 1.12.17

--- build/go/build-112.sh
+++ build/go/build-112.sh
@@ -18,7 +18,7 @@
 
 PROG=go
 PKG=ooce/developer/go-112
-VER=1.12.16
+VER=1.12.17
 SUMMARY="The Go Programming Language"
 DESC="An open source programming language that makes it easy to build simple, "
 DESC+="reliable, and efficient software."
