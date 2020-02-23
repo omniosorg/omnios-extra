cb5d2a85769bf81b6dde7ac749e64f4b9f9bbde6
Dominik Hassler <hadfl@omniosce.org>
2020-02-23 09:51:34 +0000

    virtualbox-5: update to 5.2.38

--- build/virtualbox/build5.sh
+++ build/virtualbox/build5.sh
@@ -18,8 +18,8 @@
 
 PROG=VirtualBox
 PKG=ooce/virtualization/virtualbox  ##IGNORE##
-VER=5.2.36
-GSOAPVER=2.8.97
+VER=5.2.38
+GSOAPVER=2.8.98
 GSOAPDIR=gsoap-${GSOAPVER%.*}
 SUMMARY="VirtualBox"
 DESC="VirtualBox is a general-purpose full virtualiser for x86 hardware, "
