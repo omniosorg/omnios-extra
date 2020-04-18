efe59374b097a0e230b28f269ab44f220e1ad29d
philip <philip@pbdigital.org>
2020-04-18 14:59:29 +0200

    cleanup fcgiwrap-1.1.0 build files

--- build/fcgiwrap/build.sh
+++ build/fcgiwrap/build.sh
@@ -18,7 +18,7 @@
 
 PROG=fcgiwrap
 VER=1.1.0
-PKG=ooce/application/$PROG
+PKG=ooce/application/fcgiwrap
 SUMMARY="Simple FastCGI wrapper for CGI scripts"
 DESC="fcgiwrap is a simple server for running CGI applications \
 over FastCGI. It hopes to provide clean CGI support to Nginx \
@@ -26,37 +26,45 @@ over FastCGI. It hopes to provide clean CGI support to Nginx \
 
 set_arch 64
 
-SKIP_LICENCES=NOSEK
-
 XFORM_ARGS="
     -DPREFIX=${PREFIX#/}
     -DPROG=$PROG
     -DVER=$VER
 "
 
-BUILD_DEPENDS_IPS="
-    ooce/library/fcgi2
-"
-RUN_DEPENDS_IPS="
+BUILD_DEPENDS_IPS+="
     ooce/library/fcgi2
 "
 
-CONFIGURE_OPTS_64=" 
-    --prefix=$PREFIX
+# The packaged Makefile.in will install the final binary into
+# $(prefix)$(sbindir) so override the fully-qualified --sbindir that comes
+# in from the framework. configure does check if the argument is fully
+# qualified hence the leading /
+CONFIGURE_OPTS="
     --without-systemd
+    --sbindir=/sbin
 "
 
+# configure does not properly use pkg-config for these.
 CFLAGS+=" -I$PREFIX/include"
-LDFLAGS+=" -L$PREFIX/lib -R$PREFIX/lib -lsocket"
+LDFLAGS64+=" -L$PREFIX/lib/$ISAPART64 -R$PREFIX/lib/$ISAPART64 -lsocket"
+
+extract_licence() {
+    logmsg "-- extracting licence"
+    pushd $TMPDIR/$BUILDDIR > /dev/null
+    sed '/^$/q' < $PROG.c > LICENCE
+    popd > /dev/null
+}
 
 init
-download_source $PROG $PROG $VER
+download_source $PROG $VER
+extract_licence
 patch_source
 run_autoreconf -i
 prep_build
 build
 strip_install
-install_smf application $PROG.xml application-$PROG
+install_smf application $PROG.xml
 make_package
 clean_up
 
