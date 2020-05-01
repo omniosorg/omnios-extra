e9685aececee364fcbaac9e64ea1188c04fd269f
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-05-01 00:24:38 +0000

    add nagios-plugins-2.3.3 build files

---
+++ build/nagios-plugins/build.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2020 OmniOS Community Edition.  All rights reserved.
+
+. ../../lib/functions.sh
+
+PROG=nagios-plugins
+VER=2.3.3
+PKG=ooce/application/nagios-plugins
+SUMMARY="Plugins for Nagios"
+DESC="This is the nagios-plugins package for Nagios."
+
+set_arch 64
+
+BUILD_DEPENDS_IPS+="
+    ooce/database/mariadb-104
+    ooce/database/postgresql-12
+"
+
+RUN_DEPENDS_IPS+="
+    ?pkg:/ooce/database/mariadb-104
+    ?pkg:/ooce/database/postgresql-12
+"
+
+OPREFIX=$PREFIX
+PREFIX+="/$PROG"
+
+MAKE_INSTALL_TARGET="
+    install
+    install-root
+"
+
+CONFIGURE_OPTS_64=" 
+    --prefix=$PREFIX
+"
+
+CFLAGS64+=" -I/opt/ooce/pgsql-12/include"
+LDFLAGS64+=" -L/opt/ooce/pgsql-12/lib -R/opt/ooce/pgsql-12/lib"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+strip_install
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
