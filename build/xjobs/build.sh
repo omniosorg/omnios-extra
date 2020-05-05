ee7609f77d02694d3e9343c20bc71c05f85b33dd
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-05-05 20:21:49 +0000

    Add xjobs

---
+++ build/xjobs/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/functions.sh
+
+PROG=xjobs
+VER=20191217
+PKG=ooce/util/xjobs
+SUMMARY="xjobs - execute jobs in parallel"
+DESC="xjobs reads job descriptions line by line and executes them in parallel"
+
+set_arch 64
+
+CONFIGURE_OPTS+=" --sysconfdir=/etc/$PREFIX/$PROG"
+CFLAGS+=" -I/usr/include/ncurses"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
