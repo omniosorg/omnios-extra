94848bb9f20db6af96251199820a129467653c98
philip <philip@pbdigital.org>
2020-05-30 11:51:35 +0200

    pkg node updated to 12.17.0

--- build/nodejs/build-12.sh
+++ build/nodejs/build-12.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=node
-VER=12.16.3
+VER=12.17.0
 PKG=ooce/runtime/node-12
 SUMMARY="Node.js is an evented I/O framework for the V8 JavaScript engine."
 DESC="Node.js is an evented I/O framework for the V8 JavaScript engine. "
