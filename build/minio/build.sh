a3414e6d3a4970b870964daa72e088682bec5235
Dominik Hassler <hadfl@omniosce.org>
2020-07-26 11:47:36 +0000

    minio: update to 2020-07-24T22-43-05Z

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -18,7 +18,7 @@
 
 PROG=minio
 PKG=ooce/storage/minio
-VER=2020-07-14T19-14-30Z
+VER=2020-07-24T22-43-05Z
 SUMMARY="MinIO server"
 DESC="A high Performance Object Storage released under Apache License v2.0. "
 DESC+="It is API compatible with Amazon S3 cloud storage service."
