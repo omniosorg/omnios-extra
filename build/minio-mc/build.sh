dd5d4fb0b94a3335093d7170ce34573426950adc
Dominik Hassler <hadfl@omniosce.org>
2020-08-09 10:01:36 +0000

    minio-mc: update to 2020-08-08T02-33-58Z

--- build/minio-mc/build.sh
+++ build/minio-mc/build.sh
@@ -18,7 +18,7 @@
 
 PROG=minio-mc
 PKG=ooce/storage/minio-mc
-VER=2020-07-31T23-34-13Z
+VER=2020-08-08T02-33-58Z
 SUMMARY="MinIO client"
 DESC="A modern alternative to UNIX commands like ls, cat, cp, mirror, diff, "
 DESC+="find etc. It supports filesystems and Amazon S3 compatible cloud "
