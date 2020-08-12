c4ab4303d57e604352aec8796ebd32d80e418f6a
Philip Brown <philip@pbdigital.org>
2020-08-12 13:41:59 +0200

    update mariadb-103

--- build/mariadb/build-103.sh
+++ build/mariadb/build-103.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=mariadb
-VER=10.3.23
+VER=10.3.24
 PKG=ooce/database/mariadb-103
 SUMMARY="MariaDB"
 DESC="A community-developed, commercially supported fork of the "
