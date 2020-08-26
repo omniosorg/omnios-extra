ce97a3d3e1d69ab0252e8d51680569db44d3aec7
Dominik Hassler <hadfl@omniosce.org>
2020-08-26 15:45:06 +0000

    vagrant: update to 2.2.10

--- build/vagrant/build.sh
+++ build/vagrant/build.sh
@@ -18,7 +18,7 @@
 
 PROG=vagrant
 PKG=ooce/application/vagrant
-VER=2.2.9
+VER=2.2.10
 # latest release from https://github.com/hashicorp/vagrant-installers/releases
 INSTVER=2.2.6
 SUMMARY="Vagrant"
