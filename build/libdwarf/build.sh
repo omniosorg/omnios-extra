290121ea4de9e928288a276162734221b66869e3
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-10-25 22:53:36 +0000

    Add libdwarf

---
+++ build/libdwarf/build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/functions.sh
+
+PROG=libdwarf
+VER=20201020
+PKG=ooce/library/libdwarf
+SUMMARY="Dwarf Library and utility"
+DESC="library for accessing DWARF debug info in ELF objects"
+
+SKIP_LICENCES='*'
+
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+    -DPROG=$PROG
+"
+
+CONFIGURE_OPTS="
+    --prefix=$PREFIX
+    --enable-shared
+    --disable-static
+"
+CONFIGURE_OPTS_64+="
+    --bindir=$PREFIX/bin
+"
+
+init
+download_source $PROG $PROG $VER
+prep_build
+patch_source
+build -ctf
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
