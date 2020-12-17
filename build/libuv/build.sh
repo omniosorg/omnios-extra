222d4ea75e1171dc5e059bf76b751acc9287e469
Dominik Hassler <hadfl@omnios.org>
2020-12-17 16:58:53 +0000

    adding libuv

---
+++ build/libuv/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/functions.sh
+
+PROG=libuv
+VER=1.40.0
+PKG=ooce/library/libuv
+SUMMARY=$PROG
+DESC="Multi-platform support library with a focus on asynchronous I/O."
+
+CONFIGURE_OPTS+="
+    --disable-static
+"
+
+TESTSUITE_FILTER='^ok |^not ok|failed'
+
+init
+download_source $PROG v$VER
+patch_source
+prep_build
+run_inbuild "./autogen.sh"
+build -ctf
+run_testsuite check
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
