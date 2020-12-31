74ce7a103af572c5423f22f8e6e3a2b16ce0e011
Dominik Hassler <hadfl@omnios.org>
2020-12-31 14:07:00 +0000

    smartmontools: update to 7.2

--- build/smartmontools/build.sh
+++ build/smartmontools/build.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2019 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/functions.sh
 
 PROG=smartmontools
-VER=7.1
+VER=7.2
 VERHUMAN=$VER
 PKG=ooce/system/smartmontools
 SUMMARY="smartmontools"
@@ -27,21 +27,20 @@ RUN_DEPENDS_IPS=ooce/security/gnupg
 
 OPREFIX=$PREFIX
 PREFIX+="/$PROG"
+
 XFORM_ARGS="
     -DPREFIX=${PREFIX#/}
     -DOPREFIX=${OPREFIX#/}
     -DPROG=$PROG
+    -DPKGROOT=$PROG
 "
 
 # Build 64-bit only and skip the arch-specific directories
 set_arch 64
 
-CPPFLAGS64+=" -D_AVL_H"
 CONFIGURE_OPTS="
-    --sysconfdir=/etc$PREFIX
-    --sbindir=$PREFIX/sbin
-    --with-gnupg=/opt/ooce/bin/gpg
-    --with-scriptpath=/usr/bin:/opt/ooce/bin
+    --with-gnupg=$OOCEBIN/gpg
+    --with-scriptpath=$USRBIN:$OOCEBIN
 "
 
 init
@@ -49,7 +48,8 @@ download_source $PROG $PROG $VER
 patch_source
 prep_build
 build -noctf    # C++
-install_smf system system-smartd.xml
+strip_install
+install_smf ooce system-smartd.xml
 make_package
 clean_up
 
