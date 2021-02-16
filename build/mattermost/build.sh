4b26be3e96cceec90557f3a71ac9d71eab8a7f15
Dominik Hassler <hadfl@omnios.org>
2021-02-16 17:25:01 +0000

    mm: update to 5.32.0

--- build/mattermost/build.sh
+++ build/mattermost/build.sh
@@ -17,8 +17,8 @@
 . ../../lib/functions.sh
 
 PROG=mattermost
-VER=5.31.1
-MMCTLVER=5.31.0
+VER=5.32.0
+MMCTLVER=5.32.0
 PKG=ooce/application/mattermost
 SUMMARY="$PROG"
 DESC="All your team communication in one place, "
