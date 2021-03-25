663f70b8a00803df5bfda2540cc62afe06424e32
Dominik Hassler <hadfl@omnios.org>
2021-03-25 17:23:27 +0000

    zadm: bundle noVNC

--- build/zadm/build.sh
+++ build/zadm/build.sh
@@ -22,11 +22,14 @@ PKG=ooce/util/zadm
 SUMMARY="zone admin tool"
 DESC="$PROG - $SUMMARY"
 
+NOVNCVER=1.2.0
+
 if [ $RELVER -lt 151034 ]; then
     logmsg "--- $PKG is not built for r$RELVER"
     exit 0
 fi
 
+_MIRROR=$MIRROR
 set_mirror "$OOCEGITHUB/$PROG/releases/download"
 
 RUN_DEPENDS_IPS="network/socat"
@@ -36,6 +39,8 @@ PREFIX+="/$PROG"
 
 set_arch 64
 
+SKIP_LICENCES=MPLv2
+
 # the Zstd module contains BMI instructions even when built on an older CPU
 BMI_EXPECTED=1
 
@@ -44,6 +49,7 @@ XFORM_ARGS="
     -DOPREFIX=${OPREFIX#/}
     -DPROG=$PROG
     -DPKGROOT=$PROG
+    -DNOVNCVER=$NOVNCVER
 "
 
 CONFIGURE_OPTS_64="
@@ -52,11 +58,30 @@ CONFIGURE_OPTS_64="
     --localstatedir=/var$PREFIX
 "
 
+bundle_novnc() {
+    note -n "Bundling noVNC"
+
+    set_mirror $_MIRROR
+    save_variable EXTRACTED_SRC
+    set_builddir noVNC-$NOVNCVER
+
+    download_source novnc v$NOVNCVER
+    logcmd mkdir -p $DESTDIR$PREFIX/novnc || logerr "mkdir novnc failed"
+
+    pushd $TMPDIR/$BUILDDIR >/dev/null
+    logcmd rsync -a vnc.html app core vendor $DESTDIR$PREFIX/novnc/ \
+        || logerr "rsync failed"
+    popd >/dev/null
+
+    restore_variable EXTRACTED_SRC
+}
+
 init
 download_source "v$VER" $PROG $VER
 patch_source
 prep_build
 build
+bundle_novnc
 make_package
 clean_up
 
