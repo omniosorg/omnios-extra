0e5ff873aed08071d1d001cdda0dd10517dfabc5
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-04-03 16:46:06 +0000

    Add autoconf-archive package

---
+++ build/autoconf-archive/build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/functions.sh
+
+PROG=autoconf-archive
+VER=2021.02.19
+PKG=ooce/developer/autoconf-archive
+SUMMARY="$PROG - a collection of macros for GNU Autoconf"
+DESC="$SUMMARY ($VER)"
+
+SKIP_LICENCES=Exception
+
+OPREFIX=$PREFIX
+PREFIX+="/$PROG"
+
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+    -DOPREFIX=${OPREFIX#/}
+    -DPROG=$PROG
+"
+
+set_arch 64
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
