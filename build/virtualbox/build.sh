7e5c4001e79ae66c9aa86daf1b6fa6e6bb5d45dd
Dominik Hassler <hadfl@omnios.org>
2021-04-30 21:46:42 +0000

    virtualbox: update to 6.1.22

--- build/virtualbox/build.sh
+++ build/virtualbox/build.sh
@@ -18,7 +18,7 @@
 
 PROG=VirtualBox
 PKG=ooce/virtualization/virtualbox
-VER=6.1.20
+VER=6.1.22
 # virtualbox does currently not build with later gsoap versions
 GSOAPVER=2.8.102
 GSOAPDIR=gsoap-${GSOAPVER%.*}
