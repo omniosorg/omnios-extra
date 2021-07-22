c22b242f176b1ec4ae7367732cdb56c1bbccd8cb
Dominik Hassler <hadfl@omnios.org>
2021-07-22 15:48:19 +0000

    cmake: update to 3.21.0

--- build/cmake/build.sh
+++ build/cmake/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=cmake
-VER=3.20.5
+VER=3.21.0
 PKG=ooce/developer/cmake
 SUMMARY="Build coordinator"
 DESC="An extensible system that manages the build process in a "
