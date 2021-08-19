fa75e57e27354e61c76fe86bde81351a2dd499c9
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-08-19 11:06:49 +0000

    libjpeg-turbo - update from 2.1.0 to 2.1.1

--- build/libjpeg-turbo/build.sh
+++ build/libjpeg-turbo/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=libjpeg-turbo
-VER=2.1.0
+VER=2.1.1
 PKG=ooce/library/libjpeg-turbo
 SUMMARY="libjpeg-turbo"
 DESC="SIMD-accelerated libjpeg-compatible JPEG codec library"
