728c138e6f417fc7093ed2b5df04d239fb2f85a6
Dominik Hassler <hadfl@omnios.org>
2021-09-02 17:46:58 +0000

    gitea: update to 1.15.0

--- build/gitea/build.sh
+++ build/gitea/build.sh
@@ -18,7 +18,7 @@
 
 PROG=gitea
 PKG=ooce/application/gitea
-VER=1.14.6
+VER=1.15.0
 SUMMARY="Git with a cup of tea"
 DESC="Git with a cup of tea, painless self-hosted git service"
 
@@ -26,7 +26,7 @@ OPREFIX=$PREFIX
 PREFIX+=/$PROG
 
 set_arch 64
-set_gover 1.16
+set_gover 1.17
 # gitea 1.11.x requires node.js
 set_nodever 14
 
