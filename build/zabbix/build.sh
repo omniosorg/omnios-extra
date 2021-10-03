ecaeba8287793b7d27d732f22ffb29de65ef9531
Dominik Hassler <hadfl@omnios.org>
2021-10-03 12:18:09 +0000

    zabbix: update to 5.4.5

--- build/zabbix/build.sh
+++ build/zabbix/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=zabbix
-VER=5.4.4
+VER=5.4.5
 PKG=ooce/application/zabbix
 SUMMARY="enterprise-class open source distributed monitoring solution"
 DESC="Zabbix is software that monitors numerous parameters of a network "
