126260700839c409ebafe928288f9aedcd826881
Dominik Hassler <hadfl@omnios.org>
2021-10-03 13:29:15 +0000

    liberation-fonts: update to 2.1.5

--- build/liberation-fonts/build.sh
+++ build/liberation-fonts/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/functions.sh
 
 PROG=liberation-fonts
-VER=2.1.4
+VER=2.1.5
 PKG=ooce/fonts/liberation
 SUMMARY="Liberation fonts"
 DESC="A collection which aims to provide document layout compatibility "
