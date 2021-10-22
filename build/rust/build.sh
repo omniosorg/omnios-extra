f8c6022d55184f881c1f19bf743964550e03e607
Dominik Hassler <hadfl@omnios.org>
2021-10-22 15:10:46 +0000

    rust: update to 1.56.0

--- build/rust/build.sh
+++ build/rust/build.sh
@@ -18,7 +18,7 @@
 
 PROG=rust
 PKG=ooce/developer/rust
-VER=1.55.0
+VER=1.56.0
 SUMMARY="Rust systems programming language"
 DESC="Rust is a systems programming language that runs blazingly fast, "
 DESC+="prevents segfaults, and guarantees thread safety."
@@ -29,7 +29,7 @@ DESC+="prevents segfaults, and guarantees thread safety."
 #
 # to use bundled LLVM instead of the system one, set BUNDLED_LLVM
 #
-LLVM_MAJVER=12.0
+[ $RELVER -ge 151036 ] && LLVM_MAJVER=13.0 || LLVM_MAJVER=12.0
 
 BUILDDIR=${PROG}c-${VER}-src
 
