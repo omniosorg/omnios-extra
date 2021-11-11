fe005248271e45a0dc85940339501e90b5989c5e
Philip Brown <philip@pbdigital.org>
2021-11-11 12:56:37 +0100

    update minio-2021-10-27T16-29-42Z -> 2021-11-09T03-21-45Z

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -18,7 +18,7 @@
 
 PROG=minio
 PKG=ooce/storage/minio
-VER=2021-10-27T16-29-42Z
+VER=2021-11-09T03-21-45Z
 SUMMARY="MinIO server"
 DESC="A high Performance Object Storage released under Apache License v2.0. "
 DESC+="It is API compatible with Amazon S3 cloud storage service."
