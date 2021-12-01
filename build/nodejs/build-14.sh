a855380607f528dd343bc98910c4f57daab213b3
Dominik Hassler <hadfl@omnios.org>
2021-12-01 17:27:46 +0000

    node-14: update to 14.18.2

--- build/nodejs/build-14.sh
+++ build/nodejs/build-14.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=node
-VER=14.18.1
+VER=14.18.2
 PKG=ooce/runtime/node-14
 SUMMARY="Node.js is an evented I/O framework for the V8 JavaScript engine."
 DESC="Node.js is an evented I/O framework for the V8 JavaScript engine. "
