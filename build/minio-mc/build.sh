b60a6ad852dcc23223d895826c3d1b249b259972
Philip Brown <philip@pbdigital.org>
2021-12-25 09:15:14 +0100

    update minio-mc-2021-12-16T23-38-39Z -> 2021-12-20T23-43-34Z

--- build/minio-mc/build.sh
+++ build/minio-mc/build.sh
@@ -18,7 +18,7 @@
 
 PROG=minio-mc
 PKG=ooce/storage/minio-mc
-VER=2021-12-16T23-38-39Z
+VER=2021-12-20T23-43-34Z
 SUMMARY="MinIO client"
 DESC="A modern alternative to UNIX commands like ls, cat, cp, mirror, diff, "
 DESC+="find etc. It supports filesystems and Amazon S3 compatible cloud "
