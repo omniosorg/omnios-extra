fc1cabad6a0ffbed90e1c892ad86dfd484e7125b
Philip <philip@pbdigital.org>
2022-01-13 09:57:49 +0000

    update node-14.18.2 -> 14.18.3

--- build/nodejs/build-14.sh
+++ build/nodejs/build-14.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=node
-VER=14.18.2
+VER=14.18.3
 PKG=ooce/runtime/node-14
 SUMMARY="Node.js is an evented I/O framework for the V8 JavaScript engine."
 DESC="Node.js is an evented I/O framework for the V8 JavaScript engine. "
