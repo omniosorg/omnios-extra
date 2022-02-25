c36ec8e4a4e2a9d3cd41df2a122fc65a91b3ad1d
Andy Fiddaman <omnios@citrus-it.co.uk>
2022-02-25 13:16:48 +0000

    Add htmlq package

---
+++ build/htmlq/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/build.sh
+
+PROG=htmlq
+VER=0.4.0
+PKG=ooce/util/htmlq
+SUMMARY="Like jq, but for HTML"
+DESC="Uses CSS selectors to extract bits of content from HTML files."
+
+BUILD_DEPENDS_IPS=ooce/developer/rust
+
+set_arch 64
+
+init
+download_source $PROG v$VER
+patch_source
+prep_build
+build_rust
+install_rust
+strip_install
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
