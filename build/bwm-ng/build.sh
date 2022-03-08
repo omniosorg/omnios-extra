0412faae7abb2235ac4d1a10de75c2f2a1017106
Dave Eddy <dave@daveeddy.com>
2022-03-08 07:37:08 -0500

    bwm-ng: add package (ver 0.6.3)

---
+++ build/bwm-ng/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/build.sh
+
+PROG=bwm-ng
+VER=0.6.3
+PKG=ooce/network/$PROG
+SUMMARY="CLI network and disk io monitor"
+DESC="small and simple console-based live network and disk io bandwidth monitor"
+
+set_arch 64
+set_mirror "$GITHUB/vgropp/$PROG/archive"
+set_checksum "none"
+
+init
+download_source v$VER $PROG $VER
+prep_build
+run_inbuild "./autogen.sh"
+build
+make_package
+clean_up
+
+# Vim hints
+## vim:ts=4:sw=4:et:fdm=marker
