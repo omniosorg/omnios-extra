3199a5802a027df0c897d68106b1747cc1153969
Dominik Hassler <hadfl@omnios.org>
2022-04-09 07:12:05 +0000

    icu4c: update to 71.1

--- build/icu4c/build.sh
+++ build/icu4c/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=icu4c
-VER=70.1
+VER=71.1
 PKG=ooce/library/icu4c
 SUMMARY="ICU - International Components for Unicode"
 DESC="A mature, widely used set of C/C++ libraries providing "
