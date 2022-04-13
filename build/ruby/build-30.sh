5f3f26718afef57141cb50bc2dbc22655d22dce0
Dominik Hassler <hadfl@omnios.org>
2022-04-13 16:15:59 +0000

    ruby-30: update to 3.0.4

--- build/ruby/build-30.sh
+++ build/ruby/build-30.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=ruby
-VER=3.0.3
+VER=3.0.4
 PKG=ooce/runtime/ruby-30
 SUMMARY="Ruby"
 DESC="A dynamic, open source programming language "
