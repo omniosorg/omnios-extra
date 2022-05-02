64de1357a2eb1b5a752cee7e898ad2035bdab4f6
Dominik Hassler <hadfl@omnios.org>
2022-05-02 16:21:48 +0000

    htop: update to 3.2.0

--- build/htop/build.sh
+++ build/htop/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=htop
 PKG=ooce/system/htop
-VER=3.1.2
+VER=3.2.0
 SUMMARY="htop"
 DESC="An interactive process viewer for Unix"
 
