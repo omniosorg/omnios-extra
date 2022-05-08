9d42978308ea33023dcbfac617bbaa614a926d5d
Dominik Hassler <hadfl@omnios.org>
2022-05-08 15:35:06 +0000

    switch to gcc 12 by default

--- lib/config.sh
+++ lib/config.sh
@@ -324,7 +324,8 @@ case $RELVER in
     15103[34])          DEFAULT_GCC_VER=9; ILLUMOS_GCC_VER=7 ;;
     15103[5-8])         DEFAULT_GCC_VER=10; ILLUMOS_GCC_VER=7 ;;
     151039|151040)      DEFAULT_GCC_VER=11; ILLUMOS_GCC_VER=7 ;;
-    15104*)             DEFAULT_GCC_VER=11; ILLUMOS_GCC_VER=10 ;;
+    15104[12])          DEFAULT_GCC_VER=11; ILLUMOS_GCC_VER=10 ;;
+    15104[3-9])         DEFAULT_GCC_VER=12; ILLUMOS_GCC_VER=10 ;;
     *) logerr "Unknown release '$RELVER', can't select compiler." ;;
 esac
 
