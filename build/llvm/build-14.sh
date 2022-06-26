e5ce684a111858934056aaa716c81834c7239d4b
Dominik Hassler <hadfl@omnios.org>
2022-06-26 08:58:41 +0000

    llvm-14: update to 14.0.6

--- build/llvm/build-14.sh
+++ build/llvm/build-14.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-14
-VER=14.0.5
+VER=14.0.6
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
