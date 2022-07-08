0f9d5b5306ddf5f01a83b778ce61a03eaa396b55
Martin <msamuelsson@storvix.eu>
2022-07-08 07:46:59 +0200

    tig: add package (ver 2.5.5)

---
+++ build/tig/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2022 Martin Samuelsson
+
+. ../../lib/build.sh
+
+PROG=tig
+VER=2.5.5
+PKG=ooce/application/tig
+SUMMARY="Tig: text-mode interface for Git"
+DESC="Tig is an ncurses-based text-mode interface for git. \
+    It functions mainly as a Git repository browser, but can \
+    also assist in staging changes for commit at chunk level \
+    and act as a pager for output from various Git commands."
+
+OPREFIX=$PREFIX
+PREFIX+=/$PROG
+
+set_arch 64
+
+XFORM_ARGS="
+    -DOPREFIX=${OPREFIX#/}
+    -DPREFIX=${PREFIX#/}
+    -DPROG=$PROG
+    -DPKGROOT=$PROG
+"
+
+MAKE_INSTALL_TARGET+=" install-doc-man"
+
+# create package functions
+init
+download_source $PROG $PROG $VER
+prep_build
+build
+strip_install
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
