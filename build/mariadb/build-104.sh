7212bfb8caf0b2e0e2ba141666b4342ee55a50fc
Dominik Hassler <hadfl@omnios.org>
2022-08-16 16:52:43 +0000

    mariadb-104: update to 10.4.26

--- build/mariadb/build-104.sh
+++ build/mariadb/build-104.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=mariadb
-VER=10.4.25
+VER=10.4.26
 PKG=ooce/database/mariadb-104
 SUMMARY="MariaDB"
 DESC="A community-developed, commercially supported fork of the "
@@ -35,6 +35,8 @@ VARPATH=/var$PREFIX
 RUNPATH=$VARPATH/run
 
 SKIP_RTIME_CHECK=1
+SKIP_SSP_CHECK=1
+NO_SONAME_EXPECTED=1
 
 BUILD_DEPENDS_IPS="
     ooce/developer/cmake
