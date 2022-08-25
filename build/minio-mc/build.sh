a784280daeaec62d668bd8d85b94cd293648f93d
matt <matt@fiddaman.net>
2022-08-25 17:01:25 +0000

    minio-mc: update to 2022-08-23T05-45-20Z

--- build/minio-mc/build.sh
+++ build/minio-mc/build.sh
@@ -18,7 +18,7 @@
 
 PROG=minio-mc
 PKG=ooce/storage/minio-mc
-VER=2022-08-11T00-30-48Z
+VER=2022-08-23T05-45-20Z
 SUMMARY="MinIO client"
 DESC="A modern alternative to UNIX commands like ls, cat, cp, mirror, diff, "
 DESC+="find etc. It supports filesystems and Amazon S3 compatible cloud "
