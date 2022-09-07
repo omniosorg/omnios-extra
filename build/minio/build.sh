27ff627169f9736720a87e2397cfe93e172e2a25
matt <matt@fiddaman.net>
2022-09-07 16:00:07 +0000

    minio: update to 2022-09-01T23-53-36Z

--- build/minio/build.sh
+++ build/minio/build.sh
@@ -18,7 +18,7 @@
 
 PROG=minio
 PKG=ooce/storage/minio
-VER=2022-08-26T19-53-15Z
+VER=2022-09-01T23-53-36Z
 SUMMARY="MinIO server"
 DESC="A high Performance Object Storage released under Apache License v2.0. "
 DESC+="It is API compatible with Amazon S3 cloud storage service."
