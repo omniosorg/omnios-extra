b5f634eabfb5bad79d5b560433b88b1a18f63381
Dominik Hassler <hadfl@omnios.org>
2022-09-23 15:33:55 +0000

    haproxy: update to 2.6.6

--- build/haproxy/build.sh
+++ build/haproxy/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=haproxy
-VER=2.6.5
+VER=2.6.6
 PKG=ooce/server/haproxy
 SUMMARY="HAProxy - fast and reliable http reverse proxy and load balancer"
 DESC="A TCP/HTTP reverse proxy which is particularly suited for high "
