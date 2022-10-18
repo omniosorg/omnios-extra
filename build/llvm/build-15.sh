76b0184826390d1bd547d3f9e4c5a0a3a4d57491
Dominik Hassler <hadfl@omnios.org>
2022-10-18 16:58:39 +0000

    llvm-15: update to 15.0.3

--- build/llvm/build-15.sh
+++ build/llvm/build-15.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-15
-VER=15.0.2
+VER=15.0.3
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
