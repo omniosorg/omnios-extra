199fe0a53c51cf5214fc3d96b8935fb0815f686e
Dominik Hassler <hadfl@omnios.org>
2022-10-21 15:15:57 +0000

    yasm added

---
+++ build/yasm/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/build.sh
+
+PROG=yasm
+VER=1.3.0
+PKG=ooce/developer/yasm
+SUMMARY="$PROG"
+DESC="$PROG - modular assembler"
+
+set_clangver
+forgo_isaexec
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+run_testsuite check
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
