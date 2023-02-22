2579ac9797089a7f0aceb9b7d16c43e069686df5
Dominik Hassler <hadfl@omnios.org>
2023-02-22 19:16:05 +0000

    virtualbox: update to 7.0.6

--- build/virtualbox/build.sh
+++ build/virtualbox/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=VirtualBox
 PKG=ooce/virtualization/virtualbox
-VER=7.0.4
+VER=7.0.6
 GSOAPVER=2.8.123
 GSOAPDIR=gsoap-${GSOAPVER%.*}
 SUMMARY="VirtualBox"
