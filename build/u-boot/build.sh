db6a890847cfbdf6ff5e4c10e24b025eeec43e4a
Andy Fiddaman <illumos@fiddaman.net>
2023-02-25 15:19:00 +0000

    Add u-boot package

---
+++ build/u-boot/build.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/build.sh
+
+PROG=u-boot
+PKG=ooce/util/u-boot
+VER=2023.01
+SUMMARY="Das U-Boot"
+DESC="$SUMMARY: Universal Bootloader"
+
+set_arch 64
+
+MAKE_TARGET="sandbox_defconfig tools"
+
+configure64() {
+    MAKE_ARGS_WS="
+        V=1
+        HOSTCC=\"$CC\"
+        HOSTCFLAGS=\"$CFLAGS $CFLAGS64 -I$PREFIX/include\"
+        HOSTLDLIBS=\"$LDFLAGS $LDFLAGS64 -L$PREFIX/lib/amd64 -lnsl -lsocket\"
+    "
+}
+
+make_install() {
+    typeset dst=$DESTDIR$PREFIX/$PROG
+
+    set -eE; trap 'logerr Installation failed at $BASH_LINENO' ERR
+
+    # For now, this is all that's shipped in this package, which is the single
+    # tool needed to build the arm64-gate. It will be extended as required.
+    logcmd $MKDIR -p $dst/tools
+    logcmd $CP tools/mkimage $dst/tools/
+
+    set +eE; trap - ERR
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
--- doc/packages.md
+++ doc/packages.md
@@ -216,6 +216,7 @@
 | ooce/util/rargs		| 0.3.0		| https://github.com/lotabout/rargs/releases | [omniosorg](https://github.com/omniosorg)
 | ooce/util/rlwrap		| 0.46		| https://github.com/hanslub42/rlwrap/releases | [r7st](https://github.com/r7st)
 | ooce/util/stress-ng		| 0.14.06	| https://github.com/ColinIanKing/stress-ng/tags | [omniosorg](https://github.com/omniosorg)
+| ooce/util/u-boot		| 2023.01	| https://github.com/u-boot/u-boot/tags | [omniosorg](https://github.com/omniosorg)
 | ooce/util/uefivars		| 1.0.1		| https://github.com/citrus-it/uefivars/releases | [omniosorg](https://github.com/omniosorg)
 | ooce/util/xjobs		| 20200726	| https://www.maier-komor.de/xjobs.html | [omniosorg](https://github.com/omniosorg)
 | ooce/util/zadm		| github-latest	| https://github.com/omniosorg/zadm/releases/ | [omniosorg](https://github.com/omniosorg)
