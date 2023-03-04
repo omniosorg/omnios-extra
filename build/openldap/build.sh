96ce9f5e1c3945e64192d37591bcf7965f05623e
Dominik Hassler <hadfl@omnios.org>
2023-03-04 13:00:05 +0000

    openldap: update to 2.6.4

--- build/openldap/build.sh
+++ build/openldap/build.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=openldap
-VER=2.6.3
+VER=2.6.4
 PKG=ooce/network/openldap
 SUMMARY="open-source LDAP implementation"
 DESC="Open-source implementation of the Lightweight Directory Access Protocol"
