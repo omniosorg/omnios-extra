63486f9e322dba49ce1c8e822651e55d44575eff
Dominik Hassler <hadfl@omnios.org>
2023-07-01 08:37:01 +0000

    llvm-16: update to 16.0.6

--- build/llvm/build-16.sh
+++ build/llvm/build-16.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-16
-VER=16.0.4
+VER=16.0.6
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
