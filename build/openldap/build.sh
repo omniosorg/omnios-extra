740e739af2c556083e18a0354f333a921a4e16ac
Dominik Hassler <hadfl@omnios.org>
2023-07-05 15:52:19 +0000

    aarch64: build openldap

--- build/openldap/build.sh
+++ build/openldap/build.sh
@@ -55,11 +55,7 @@ CONFIGURE_OPTS="
     --enable-crypt
     --without-cyrus-sasl
 "
-CONFIGURE_OPTS[i386]+="
-    --bindir=$PREFIX/bin/i386
-    --disable-slapd
-"
-CONFIGURE_OPTS[amd64]+="
+COMMON_SLAPD_CONFIGURE_OPTS="
     --bindir=$PREFIX/bin
     --sbindir=$PREFIX/sbin
     --libexecdir=$PREFIX/libexec
@@ -76,26 +72,34 @@ CONFIGURE_OPTS[amd64]+="
     --enable-modules
     --enable-overlays=mod
 "
+CONFIGURE_OPTS[i386]+="
+    --bindir=$PREFIX/bin/i386
+    --disable-slapd
+"
+CONFIGURE_OPTS[amd64]+=" $COMMON_SLAPD_CONFIGURE_OPTS"
+CONFIGURE_OPTS[aarch64]+=" $COMMON_SLAPD_CONFIGURE_OPTS"
+
 LDFLAGS[i386]+=" -lssp_ns"
 
 MAKE_INSTALL_ARGS+=" STRIP= STRIP_OPTS="
 
-build_manifests() {
-    manifest_start $TMPDIR/manifest.client
-    manifest_add_dir $OPREFIX/lib pkgconfig amd64 amd64/pkgconfig
-    manifest_add_dir $PREFIX/bin
-    manifest_add_dir $OPREFIX/include
-    manifest_add_dir $PREFIX/share/man man1 man3
-    manifest_add $PREFIX/share/man/man5 ldap.conf.5 ldif.5
-    manifest_add etc$PREFIX ldap.conf
-    manifest_finalise $TMPDIR/manifest.client $OPREFIX etc$OPREFIX
+pre_configure() {
+    typeset arch=$1
 
-    manifest_uniq $TMPDIR/manifest.{server,client}
-    manifest_finalise $TMPDIR/manifest.server $OPREFIX etc$OPREFIX
+    ! cross_arch $arch && return
+
+    CONFIGURE_OPTS[$arch]+="
+        --with-yielding_select=yes
+        ac_cv_func_memcmp_working=yes
+    "
+    CPPFLAGS[$arch]+=" -I${SYSROOT[$arch]}/usr/include"
 }
 
 init
 prep_build
+
+# Skip previous versions for cross compilation
+pre_build() { ! cross_arch $1; }
 
 # Build previous versions
 for pver in $PVERS; do
@@ -109,17 +113,35 @@ for pver in $PVERS; do
     build
     restore_variables BUILDDIR EXTRACTED_SRC CONFIGURE_OPTS
 done
+unset -f pre_build
+
+post_install() {
+    [ $1 = i386 ] && return
+
+    install_smf -oocemethod ooce $PROG.xml
+
+    manifest_start $TMPDIR/manifest.client
+    manifest_add_dir $OPREFIX/lib pkgconfig amd64 amd64/pkgconfig
+    manifest_add_dir $PREFIX/bin
+    manifest_add_dir $OPREFIX/include
+    manifest_add_dir $PREFIX/share/man man1 man3
+    manifest_add $PREFIX/share/man/man5 ldap.conf.5 ldif.5
+    manifest_add etc$PREFIX ldap.conf
+    manifest_finalise $TMPDIR/manifest.client $OPREFIX etc$OPREFIX
+
+    manifest_uniq $TMPDIR/manifest.{server,client}
+    manifest_finalise $TMPDIR/manifest.server $OPREFIX etc$OPREFIX
+}
 
 note -n "Building current version: $VER"
 download_source $PROG $PROG $VER
 patch_source
 build
 xform files/$PROG.xml > $TMPDIR/$PROG.xml
-install_smf -oocemethod ooce $PROG.xml
-build_manifests
 PKG=${PKG/network/library} SUMMARY+=" - clients and libraries" \
     make_package -seed $TMPDIR/manifest.client
-make_package -seed $TMPDIR/manifest.server server.mog
+[ "$FLAVOR" != libsandheaders ] \
+    && make_package -seed $TMPDIR/manifest.server server.mog
 clean_up
 
 # Vim hints
