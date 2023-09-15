f45965b1194aa8538100b6cdd869ae0c6775dc44
Marco van Wieringen <marco.van.wieringen@planets.elm.net>
2023-09-15 10:26:54 +0200

    Add postgresql 16

---
+++ build/postgresql/build-16.sh
@@ -0,0 +1,150 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/build.sh
+
+PROG=postgresql
+PKG=ooce/database/postgresql-16
+VER=16.0
+SUMMARY="PostgreSQL 16"
+DESC="The World's Most Advanced Open Source Relational Database"
+
+SKIP_LICENCES=postgresql
+
+# We want to populate the clang-related environment variables
+# and set PATH to point to the correct llvm/clang version for
+# the postgres JIT code, but we want to build with gcc.
+set_clangver
+BASEPATH=$PATH set_gccver $DEFAULT_GCC_VER
+
+MAJVER=${VER%.*}            # M.m
+sMAJVER=${MAJVER//./}       # Mm
+set_patchdir patches-$sMAJVER
+
+OPREFIX=$PREFIX
+PREFIX+=/pgsql-$MAJVER
+CONFPATH=/etc$PREFIX
+LOGPATH=/var/log$PREFIX
+VARPATH=/var$PREFIX
+RUNPATH=$VARPATH/run
+
+reset_configure_opts
+
+SKIP_RTIME_CHECK=1
+SKIP_SSP_CHECK=1
+NO_SONAME_EXPECTED=1
+
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+    -DOPREFIX=${OPREFIX#/}
+    -DPROG=$PROG
+    -DPKGROOT=pgsql-$MAJVER
+    -DMEDIATOR=$PROG -DMEDIATOR_VERSION=$MAJVER
+    -DVERSION=$MAJVER
+    -DsVERSION=$sMAJVER
+"
+
+CFLAGS+=" -O3"
+CFLAGS[aarch64]+=" -mno-outline-atomics"
+CPPFLAGS+=" -DWAIT_USE_POLL"
+# postgresql has large enumerations
+CTF_FLAGS+=" -s"
+
+CONFIGURE_OPTS="
+    --prefix=$PREFIX
+    --sysconfdir=$CONFPATH
+    --localstatedir=$VARPATH
+    --enable-thread-safety
+    --with-pam
+    --with-ssl=openssl
+    --with-lz4
+    --with-zstd
+    --with-libxml
+    --with-libxslt
+    --with-readline
+    --without-systemd
+    --with-system-tzdata=/usr/share/lib/zoneinfo
+"
+
+CONFIGURE_OPTS[amd64_WS]+="
+    --bindir=$PREFIX/bin
+    --with-llvm LLVM_CONFIG=\"$CLANGPATH/bin/llvm-config --link-static\"
+    --enable-dtrace DTRACEFLAGS=-64
+"
+CONFIGURE_OPTS[aarch64]+="
+    --disable-dtrace
+"
+
+# need to build world to get e.g. man pages in
+MAKE_TARGET=world
+MAKE_INSTALL_TARGET=install-world
+
+pre_configure() {
+    typeset arch=$1
+
+    LDFLAGS[$arch]+=" -Wl,-R$OPREFIX/${LIBDIRS[$arch]}"
+}
+
+post_install() {
+    typeset arch=$1
+
+    [ $arch = i386 ] && return
+
+    # Make ISA binaries for pg_config, to allow software to find the
+    # right settings for 32/64-bit when pkg-config is not used.
+    if [ $arch = amd64 ]; then
+        pushd $DESTDIR$PREFIX/bin >/dev/null
+        logcmd mkdir -p amd64
+        logcmd mv pg_config amd64/ || logerr "mv pg_config"
+        make_isaexec_stub_arch amd64 $PREFIX/bin
+        popd >/dev/null
+    fi
+
+    xform $SRCDIR/files/postgres.xml > $TMPDIR/$PROG-$sMAJVER.xml
+    install_smf ooce $PROG-$sMAJVER.xml
+
+    manifest_start $TMPDIR/manifest.client
+    manifest_add_dir $PREFIX/include '.*'
+    manifest_add_dir $PREFIX/lib/pkgconfig
+    manifest_add_dir $PREFIX/lib/amd64/pkgconfig
+    manifest_add_dir $PREFIX/lib/pgxs '.*'
+    manifest_add_dir $PREFIX/lib/amd64/pgxs '.*'
+    manifest_add $PREFIX/lib '.*lib(pq\.|ecpg|pgtypes|pgcommon|pgport).*'
+    manifest_add $PREFIX/bin '.*pg_config' psql ecpg
+    manifest_add $PREFIX/share/man/man1 pg_config.1 psql.1 ecpg.1
+    manifest_add $PREFIX/share psqlrc.sample
+    manifest_finalise $TMPDIR/manifest.client $OPREFIX
+
+    manifest_uniq $TMPDIR/manifest.{server,client}
+    manifest_finalise $TMPDIR/manifest.server $OPREFIX etc
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+#run_testsuite check-world
+PKG=${PKG/database/library} SUMMARY+=" client and libraries" \
+    make_package -seed $TMPDIR/manifest.client
+[ "$FLAVOR" != libsandheaders ] \
+    && RUN_DEPENDS_IPS="ooce/database/postgresql-common" \
+        make_package -seed $TMPDIR/manifest.server server.mog
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
--- doc/packages.md
+++ doc/packages.md
@@ -42,6 +42,7 @@
 | ooce/database/postgresql-13	| 13.12		| https://www.postgresql.org/ftp/source/ | [omniosorg](https://github.com/omniosorg)
 | ooce/database/postgresql-14	| 14.9		| https://www.postgresql.org/ftp/source/ | [omniosorg](https://github.com/omniosorg)
 | ooce/database/postgresql-15	| 15.4		| https://www.postgresql.org/ftp/source/ | [omniosorg](https://github.com/omniosorg)
+| ooce/database/postgresql-16	| 16.0		| https://www.postgresql.org/ftp/source/ | [omniosorg](https://github.com/omniosorg)
 | ooce/database/postgresql-XX/citus	| 11.2.0	| https://github.com/citusdata/citus/releases | [omniosorg](https://github.com/omniosorg)
 | ooce/database/postgresql-XX/mysql_fdw	| 2.9.0	| https://github.com/EnterpriseDB/mysql_fdw/releases | [omniosorg](https://github.com/omniosorg)
 | ooce/database/postgresql-XX/pg_repack	| 1.4.8	| https://github.com/reorg/pg_repack/tags | [omniosorg](https://github.com/omniosorg)
