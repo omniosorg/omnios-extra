5b925d7dceaa1241605e9de5c98fc5a39eb337f1
Dominik Hassler <hadfl@omnios.org>
2023-10-21 14:22:50 +0000

    mosquitto: update to 2.0.18

--- build/mosquitto/build.sh
+++ build/mosquitto/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=mosquitto
-VER=2.0.17
+VER=2.0.18
 PKG=ooce/network/mosquitto
 SUMMARY="$PROG - an open source message broker"
 DESC="$PROG is an open source (EPL/EDL licensed) message broker that "
