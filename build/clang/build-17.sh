443ef26deb1ebead1d3f31350ce018956e82143f
Dominik Hassler <hadfl@omnios.org>
2023-10-31 17:40:58 +0000

    clang-17: update to 17.0.4

--- build/clang/build-17.sh
+++ build/clang/build-17.sh
@@ -18,7 +18,7 @@
 
 PROG=clang
 PKG=ooce/developer/clang-17
-VER=17.0.3
+VER=17.0.4
 SUMMARY="C language family frontend for LLVM"
 DESC="The Clang project provides a language front-end and tooling "
 DESC+="infrastructure for languages in the C language family (C, C++, "
