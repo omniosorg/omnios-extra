a2bb477f979dd422cfd0b9a6ff9af94a54814a7a
Dominik Hassler <hadfl@omnios.org>
2024-01-12 16:12:35 +0000

    pixman: update to 0.43.0

--- build/pixman/build.sh
+++ build/pixman/build.sh
@@ -12,27 +12,44 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=pixman
-VER=0.42.2
+VER=0.43.0
 PKG=ooce/library/pixman
 SUMMARY="$PROG"
 DESC="Low-level software library for pixel manipulation, providing features "
 DESC+="such as image compositing and trapezoid rasterization"
 
-CONFIGURE_OPTS="--disable-static"
+TESTSUITE_SED='
+    /Running all tests/d
+    s/  *[0-9][0-9.]*s//
+    /^Full log written to/d
+'
 
 CFLAGS[aarch64]+=" -mtls-dialect=trad"
+
+pre_configure() {
+    typeset arch=$1
+
+    CONFIGURE_OPTS[$arch]="
+        --prefix=$PREFIX
+        --libdir=$PREFIX/${LIBDIRS[$arch]}
+    "
+
+    ! cross_arch $arch && return
+
+    CONFIGURE_CMD+=" --cross-file $SRCDIR/files/aarch64-gcc.txt"
+}
 
 init
 download_source $PROG $PROG $VER
 patch_source
-prep_build
+prep_build meson
 build
-run_testsuite check
+run_testsuite
 make_package
 clean_up
 
