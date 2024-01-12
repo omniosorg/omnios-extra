ed27c57c70ccda66362e63d5be5fed7ff2994b07
Dominik Hassler <hadfl@omnios.org>
2024-01-12 16:13:34 +0000

    node package updates

--- build/nodejs/build-20.sh
+++ build/nodejs/build-20.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=node
-VER=20.10.0
+VER=20.11.0
 PKG=ooce/runtime/node-20
 SUMMARY="Node.js is an evented I/O framework for the V8 JavaScript engine."
 DESC="Node.js is an evented I/O framework for the V8 JavaScript engine. "
