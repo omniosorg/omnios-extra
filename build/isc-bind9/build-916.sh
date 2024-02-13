cce0d0da48889b1d0e2af3b0de216725ce77bc70
Andy Fiddaman <illumos@fiddaman.net>
2024-02-13 13:52:48 +0000

    bind: update to 9.16.48 / 9.18.24

--- build/isc-bind9/build-916.sh
+++ build/isc-bind9/build-916.sh
@@ -13,12 +13,12 @@
 # }}}
 #
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=bind
-VER=9.16.45
+VER=9.16.48
 PKG=ooce/network/bind-916
 SUMMARY="ISC BIND DNS Server & Tools"
 DESC="Server & Client Utilities for DNS"
