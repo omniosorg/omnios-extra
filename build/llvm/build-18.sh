9bcd0578062b00095c7201bfecd5683267efe5f1
Dominik Hassler <hadfl@omnios.org>
2024-03-23 14:27:27 +0000

    llvm-18: update to 18.1.2

--- build/llvm/build-18.sh
+++ build/llvm/build-18.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-18
-VER=18.1.0
+VER=18.1.2
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
