a31798477e75803902a342324514e2d73bf42ec8
Philip Brown <philip.brown@hyvanix.com>
2024-04-09 14:44:56 +0000

    haproxy: update to 2.8.9

--- build/haproxy/build.sh
+++ build/haproxy/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=haproxy
-VER=2.8.7
+VER=2.8.9
 PKG=ooce/server/haproxy
 SUMMARY="HAProxy - fast and reliable http reverse proxy and load balancer"
 DESC="A TCP/HTTP reverse proxy which is particularly suited for high "
