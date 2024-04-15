a969107f1963e7824cf345303570a7e82e511e8a
Philip Brown <philip.brown@hyvanix.com>
2024-04-15 14:05:10 +0000

    php 8.1: update to 8.1.28

--- build/php/build-81.sh
+++ build/php/build-81.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=php
 PKG=ooce/application/php-81
-VER=8.1.27
+VER=8.1.28
 SUMMARY="PHP 8.1"
 DESC="A popular general-purpose scripting language"
 
