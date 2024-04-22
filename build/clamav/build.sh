d5c6d5e314a18d272db217f3dc490b85ed26105f
Philip Brown <philip.brown@hyvanix.com>
2024-04-22 12:19:04 +0000

    clam av: update to 1.3.1

--- build/clamav/build.sh
+++ build/clamav/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=clamav
-VER=1.3.0
+VER=1.3.1
 PKG=ooce/system/clamav
 SUMMARY="Clam Anti-virus"
 DESC="$PROG is an open-source antivirus engine for detecting trojans, "
