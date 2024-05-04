8729966a30202b2de86a70f16a8e5ed5f03c9637
Dominik Hassler <hadfl@omnios.org>
2024-05-04 10:29:26 +0000

    virtualbox: update to 7.0.18

--- build/virtualbox/build.sh
+++ build/virtualbox/build.sh
@@ -18,8 +18,8 @@
 
 PROG=VirtualBox
 PKG=ooce/virtualization/virtualbox
-VER=7.0.16
-GSOAPVER=2.8.129
+VER=7.0.18
+GSOAPVER=2.8.134
 GSOAPDIR=gsoap-${GSOAPVER%.*}
 SUMMARY="VirtualBox"
 DESC="VirtualBox is a general-purpose full virtualiser for x86 hardware, "
