1ed09f5e0cdff068fcd07d8ec995b988507a0fec
Brian Ledbetter <brian.ledbetter@efiia.com>
2024-05-20 21:07:25 -0400

    Update copyright date

--- build/sdns/build.sh
+++ build/sdns/build.sh
@@ -12,7 +12,7 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
