680eb8e2a5a301771c12776f045f62cd712fc34a
Dominik Hassler <hadfl@omnios.org>
2024-07-27 12:01:02 +0000

    adding liburcu

---
+++ build/liburcu/build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
+
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
+
+. ../../lib/build.sh
+
+PROG=liburcu
+VER=0.14.0
+PKG=ooce/library/liburcu
+SUMMARY="Userspace RCU"
+DESC="Userspace RCU (read-copy-update) library. This data synchronization "
+DESC+="library provides read-side access which scales linearly with the "
+DESC+="number of cores."
+
+set_clangver
+
+set_builddir userspace-rcu-$VER
+
+XFORM_ARGS="
+    -DPREFIX=${PREFIX#/}
+"
+
+# configure complains about make not being GNU make
+export MAKE
+
+CONFIGURE_OPTS="--disable-static"
+
+pre_configure() {
+    typeset arch=$1
+
+    LDFLAGS[$arch]+=" -Wl,-R$PREFIX/${LIBDIRS[$arch]}"
+}
+
+CFLAGS[aarch64]+=" -mtls-dialect=trad"
+
+init
+download_source $PROG userspace-rcu $VER
+patch_source
+prep_build
+build
+run_testsuite check
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:fdm=marker
--- doc/packages.md
+++ doc/packages.md
@@ -128,6 +128,7 @@
 | ooce/library/libpciaccess	| 0.18		| https://xorg.freedesktop.org/archive/individual/lib | [drscream](https://github.com/drscream)
 | ooce/library/libpng		| 1.6.43	| http://www.libpng.org/pub/png/libpng.html | [omniosorg](https://github.com/omniosorg)
 | ooce/library/libsodium	| 1.0.20	| https://github.com/jedisct1/libsodium/releases | [omniosorg](https://github.com/omniosorg)
+| ooce/library/liburcu		| 0.14.0	| https://lttng.org/files/urcu/?O=D | [omniosorg](https://github.com/omniosorg)
 | ooce/library/libusb-1		| 1.0.27	| https://github.com/libusb/libusb/releases | [omniosorg](https://github.com/omniosorg)
 | ooce/library/libuv		| 1.48.0	| https://github.com/libuv/libuv/releases | [omniosorg](https://github.com/omniosorg)
 | ooce/library/libvorbis	| 1.3.7		| https://ftp.osuosl.org/pub/xiph/releases/vorbis/ | [omniosorg](https://github.com/omniosorg)
