9fc80e42fb8c026d4ea5df3d3e72effe22f291cd
Dominik Hassler <hadfl@omnios.org>
2024-11-09 11:38:26 +0000

    go package updates

--- build/go/build-123.sh
+++ build/go/build-123.sh
@@ -18,7 +18,7 @@
 
 PROG=go
 PKG=ooce/developer/go-123
-VER=1.23.2
+VER=1.23.3
 SUMMARY="The Go Programming Language"
 DESC="An open source programming language that makes it easy to build simple, "
 DESC+="reliable, and efficient software."
