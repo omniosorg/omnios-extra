4ea6794e73acde4fb52d4498701d3603d85d59ec
Dominik Hassler <hadfl@omnios.org>
2024-11-09 11:34:32 +0000

    fix postgresql builds with gcc 14

--- build/postgresql/build-13.sh
+++ build/postgresql/build-13.sh
@@ -23,9 +23,6 @@ VER=13.16
 SUMMARY="PostgreSQL 13"
 DESC="The World's Most Advanced Open Source Relational Database"
 
-# does not yet build with gcc 14
-((GCCVER > 13)) && set_gccver 13
-
 SKIP_LICENCES=postgresql
 
 MAJVER=${VER%.*}            # M.m
