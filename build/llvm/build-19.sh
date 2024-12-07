e3c7e806b8d789713f695fed78f10f820943306e
Dominik Hassler <hadfl@omnios.org>
2024-12-07 13:21:06 +0000

    llvm-19: update to 19.1.5

--- build/llvm/build-19.sh
+++ build/llvm/build-19.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-19
-VER=19.1.4
+VER=19.1.5
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
