9e9100248c8b17bfde6dc15ef90020976c1e9e0d
Mike Zeller <mike@mikezeller.net>
2025-01-05 05:50:36 +0000

    Bump helix to version 25.01

--- build/helix/build.sh
+++ build/helix/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=helix
-VER=24.07
+VER=25.01
 PKG=ooce/editor/helix
 SUMMARY="A post-modern modal text editor."
 DESC="A kakoune / neovim inspired editor, written in Rust."
