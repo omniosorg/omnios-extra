ae9fb99c9127251686d9e4a25ed02368862383ea
Dominik Hassler <hadfl@omnios.org>
2025-01-11 13:15:44 +0000

    rust: update to 1.84.0

--- build/rust/build-arch-aarch64.sh
+++ build/rust/build-arch-aarch64.sh
@@ -18,7 +18,7 @@
 
 PROG=rust
 PKG=ooce/developer/rust
-VER=1.83.0
+VER=1.84.0
 SUMMARY="Rust systems programming language"
 DESC="Rust is a systems programming language that runs blazingly fast, "
 DESC+="prevents segfaults, and guarantees thread safety."
