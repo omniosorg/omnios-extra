449081f8a05ccd644853eee44310914118af93cb
Dominik Hassler <hadfl@omnios.org>
2025-05-10 19:18:58 +0000

    node.js package updates

--- build/nodejs/build-18.sh
+++ build/nodejs/build-18.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=node
-VER=18.20.6
+VER=18.20.8
 PKG=ooce/runtime/node-18
 SUMMARY="Node.js is an evented I/O framework for the V8 JavaScript engine."
 DESC="Node.js is an evented I/O framework for the V8 JavaScript engine. "
