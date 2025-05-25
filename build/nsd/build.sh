6eb63a614091d0fdff7d14cf7866e834a14d3082
Dominik Hassler <hadfl@omnios.org>
2025-05-25 14:12:16 +0000

    nsd: update to 4.12.0

--- build/nsd/build.sh
+++ build/nsd/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=nsd
-VER=4.11.1
+VER=4.12.0
 PKG=ooce/network/nsd
 SUMMARY="Authoritative DNS server"
 DESC="The NLnet Labs Name Server Daemon (NSD) is an authoritative "
