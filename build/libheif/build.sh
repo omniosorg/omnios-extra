b87e8f95f1282799a62841876a2d53f491e9b95e
Dominik Hassler <hadfl@omnios.org>
2025-05-25 13:30:17 +0000

    libheif: update to 1.19.8

--- build/libheif/build.sh
+++ build/libheif/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=libheif
-VER=1.19.7
+VER=1.19.8
 PKG=ooce/library/libheif
 SUMMARY="HEIF and AVIF encoder"
 DESC="ISO/IEC 23008-12:2017 HEIF and AVIF (AV1 Image File Format) "
