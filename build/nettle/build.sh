1da25010eb5750ad89d9eff5b899240df45d82b9
Dominik Hassler <hadfl@omnios.org>
2025-07-05 09:54:18 +0000

    nettle: update to 3.10.2

--- build/nettle/build.sh
+++ build/nettle/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=nettle
-VER=3.10.1
+VER=3.10.2
 PKG=ooce/library/nettle
 SUMMARY="$PROG - low-level cryptographic library"
 DESC="Cryptographic library that is designed to fit easily in more or "
