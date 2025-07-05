84bc06eb87568f62bfeeb568d8eaa6e896a26db1
Dominik Hassler <hadfl@omnios.org>
2025-07-05 09:09:42 +0000

    php package updates

--- build/php/build-83.sh
+++ build/php/build-83.sh
@@ -18,7 +18,7 @@
 
 PROG=php
 PKG=ooce/application/php-83
-VER=8.3.21
+VER=8.3.23
 SUMMARY="PHP 8.3"
 DESC="A popular general-purpose scripting language"
 
--- build/php/build-84.sh
+++ build/php/build-84.sh
@@ -18,7 +18,7 @@
 
 PROG=php
 PKG=ooce/application/php-84
-VER=8.4.7
+VER=8.4.10
 SUMMARY="PHP 8.4"
 DESC="A popular general-purpose scripting language"
 
