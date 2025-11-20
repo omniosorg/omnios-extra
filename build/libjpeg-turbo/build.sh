107cfda0124556c2a781a0ce225951dd1f43d04b
Dominik Hassler <hadfl@omnios.org>
2025-11-20 16:33:26 +0000

    libjpeg-turbo: update to 3.1.2

--- build/libjpeg-turbo/build.sh
+++ build/libjpeg-turbo/build.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=libjpeg-turbo
-VER=3.0.1
+VER=3.1.2
 PKG=ooce/library/libjpeg-turbo
 SUMMARY="libjpeg-turbo"
 DESC="SIMD-accelerated libjpeg-compatible JPEG codec library"
@@ -29,13 +29,8 @@ BUILD_DEPENDS_IPS="
 
 set_clangver
 
-OPREFIX=$PREFIX
-PREFIX+="/$PROG"
-
 XFORM_ARGS="
     -DPREFIX=${PREFIX#/}
-    -DOPREFIX=${OPREFIX#/}
-    -DPROG=$PROG
 "
 
 TESTSUITE_SED='
@@ -47,8 +42,6 @@ CONFIGURE_OPTS="
     -DCMAKE_BUILD_TYPE=Release
     -DENABLE_STATIC=0
     -DCMAKE_INSTALL_PREFIX=$PREFIX
-    -DCMAKE_INSTALL_INCLUDEDIR=$OPREFIX/include
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 "
 
 CFLAGS[aarch64]+=" -mtls-dialect=trad"
@@ -56,12 +49,13 @@ CFLAGS[aarch64]+=" -mtls-dialect=trad"
 pre_build() {
     typeset arch=$1
 
-    CONFIGURE_OPTS[$arch]="-DCMAKE_INSTALL_LIBDIR=$OPREFIX/${LIBDIRS[$arch]}"
+    CONFIGURE_OPTS[$arch]="-DCMAKE_INSTALL_LIBDIR=${LIBDIRS[$arch]}"
 
     ! cross_arch $arch && return
 
-    # XXXARM: check whether it's possible to cross-build SIMD
-     CONFIGURE_OPTS[$arch]+=" -DWITH_SIMD=OFF"
+    CONFIGURE_OPTS[$arch]+="
+        -DCMAKE_TOOLCHAIN_FILE=$SRCDIR/files/cmake-toolchain-$arch.txt
+    "
 }
 
 init
