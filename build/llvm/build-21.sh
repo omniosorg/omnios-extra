05900b9122b0767fe6ff9d5636494e90fc622401
Dominik Hassler <hadfl@omnios.org>
2025-12-05 17:19:07 +0000

    llvm-21: update to 21.1.7

--- build/llvm/build-21.sh
+++ build/llvm/build-21.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-21
-VER=21.1.6
+VER=21.1.7
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
