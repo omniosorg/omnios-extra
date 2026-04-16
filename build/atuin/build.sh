c0314888bac75b79cbcaadad84ca1c0c9cd07487
Mike Zeller <mike@mikezeller.net>
2026-04-16 18:01:03 +0000

    18.15.1

--- build/atuin/build.sh
+++ build/atuin/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=atuin
-VER=18.14.1
+VER=18.15.1
 PKG=ooce/util/atuin
 SUMMARY="Magical shell history"
 DESC="Replaces your existing shell history with a SQLite database and "
