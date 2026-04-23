a9a2e657c8c49d5739f64f33c622a68d52eca749
Dominik Hassler <hadfl@omnios.org>
2026-04-23 15:36:47 +0000

    rust: update to 1.95.0

--- build/rust/build.sh
+++ build/rust/build.sh
@@ -19,7 +19,7 @@
 
 PROG=rust
 PKG=ooce/developer/rust
-VER=1.94.1
+VER=1.95.0
 SUMMARY="Rust systems programming language"
 DESC="Rust is a systems programming language that runs blazingly fast, "
 DESC+="prevents segfaults, and guarantees thread safety."
