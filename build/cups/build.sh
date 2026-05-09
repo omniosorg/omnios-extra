87ac156af587ae91ae920b19c072b9bdc605404d
Dominik Hassler <hadfl@omnios.org>
2026-05-09 14:29:04 +0000

    cups: update to 2.4.19

--- build/cups/build.sh
+++ build/cups/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=cups
-VER=2.4.17
+VER=2.4.19
 PKG=ooce/print/cups
 SUMMARY="Common UNIX Printing System"
 DESC="Standards-based, open source printing system for UNIX operating systems"
