530c5c00399d1b1c34cb239e37c9514a6b2dce96
Dominik Hassler <hadfl@omnios.org>
2026-05-09 14:44:32 +0000

    unbound: update to 1.25.0

--- build/unbound/build.sh
+++ build/unbound/build.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2026 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=unbound
-VER=1.24.2
+VER=1.25.0
 PKG=ooce/network/unbound
 SUMMARY="DNS resolver"
 DESC="Unbound is a validating, recursive, caching DNS resolver."
