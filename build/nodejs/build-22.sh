7fb66fac094530bdcc53ff28edc8e62b34e623a6
Dominik Hassler <hadfl@omnios.org>
2026-09-11 15:31:34 +0000

    node.js package updates

--- build/nodejs/build-22.sh
+++ build/nodejs/build-22.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=node
-VER=22.23.1
+VER=22.23.2
 PKG=ooce/runtime/node-22
 SUMMARY="Node.js is an evented I/O framework for the V8 JavaScript engine."
 DESC="Node.js is an evented I/O framework for the V8 JavaScript engine. "
