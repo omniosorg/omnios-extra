4070c54d0551390fbb4f3bcd616a05a5579b060b
Dominik Hassler <hadfl@omnios.org>
2026-09-23 19:15:08 +0000

    llvm-23: update to 23.1.2

--- build/llvm/build-23.sh
+++ build/llvm/build-23.sh
@@ -18,7 +18,7 @@
 
 PROG=llvm
 PKG=ooce/developer/llvm-23
-VER=23.1.1
+VER=23.1.2
 SUMMARY="Low Level Virtual Machine compiler infrastructure"
 DESC="A collection of modular and reusable compiler and toolchain technologies"
 
